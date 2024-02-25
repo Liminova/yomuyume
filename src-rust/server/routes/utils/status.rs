3c869efca255c1580984bba77af30770dee496b4
Delnegend <kiennguyen19323@gmail.com>
2024-02-25 22:11:07 +0700

    fix(rust/routes/utils/status): :bug: replace old response with MyResponse

--- src-rust/server/routes/utils/status.rs
+++ src-rust/server/routes/utils/status.rs
@@ -1,5 +1,5 @@
-use crate::constants::version::get_version;
-use axum::{extract::Query, http::StatusCode, response::IntoResponse, Json};
+use crate::{constants::version::get_version, MyResponseBuilder};
+use axum::{extract::Query, http::HeaderMap, response::IntoResponse, Json};
 use chrono::Local;
 
 pub use bridge::routes::utils::{StatusRequest, StatusResponseBody};
@@ -7,31 +7,33 @@ pub use bridge::routes::utils::{StatusRequest, StatusResponseBody};
 #[utoipa::path(get, path = "/api/utils/status", params(StatusRequest), responses(
     (status = 200, description = "Status check successful", body = StatusResponseBody)
 ))]
-pub async fn get_status(query: Query<StatusRequest>) -> impl IntoResponse {
+pub async fn get_status(header: HeaderMap, query: Query<StatusRequest>) -> impl IntoResponse {
+    let builder = MyResponseBuilder::new(header);
+
     let echo = query.echo.clone();
     let version = get_version();
-    (
-        StatusCode::OK,
-        Json(StatusResponseBody {
-            server_time: Local::now().to_string(),
-            version,
-            echo,
-        }),
-    )
+
+    builder.success(StatusResponseBody {
+        server_time: Local::now().to_string(),
+        version,
+        echo,
+    })
 }
 
 #[utoipa::path(post, path = "/api/utils/status", responses(
     (status = 200, description = "Status check successful", body = StatusResponseBody)
 ))]
-pub async fn post_status(query: Option<Json<StatusRequest>>) -> impl IntoResponse {
+pub async fn post_status(
+    header: HeaderMap,
+    query: Option<Json<StatusRequest>>,
+) -> impl IntoResponse {
+    let builder = MyResponseBuilder::new(header);
+
     let echo = query.and_then(|q| q.echo.clone());
     let version = get_version();
-    (
-        StatusCode::OK,
-        Json(StatusResponseBody {
-            server_time: Local::now().to_string(),
-            version,
-            echo,
-        }),
-    )
+    builder.success(StatusResponseBody {
+        server_time: Local::now().to_string(),
+        version,
+        echo,
+    })
 }
