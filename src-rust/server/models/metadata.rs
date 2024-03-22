7418713901cdc0076c98f5cc2057d27fa1c64671
Delnegend <kiennguyen19323@gmail.com>
2024-03-23 01:57:58 +0700

    test(models/metadata.rs): :white_check_mark: new tests

--- src-rust/server/models/metadata.rs
+++ src-rust/server/models/metadata.rs
@@ -148,3 +148,117 @@ impl CategoryMetadata {
         }
     }
 }
+
+#[cfg(test)]
+mod tests_title {
+    use super::*;
+    use std::fs::remove_file;
+
+    #[test]
+    fn empty() {
+        let path = Path::new("test-title-empty.toml");
+        let metadata = TitleMetadata::from(path);
+
+        assert_eq!(metadata.title, None);
+        assert_eq!(metadata.description, None);
+        assert_eq!(metadata.cover, None);
+        assert_eq!(metadata.author, None);
+        assert_eq!(metadata.release, None);
+        assert_eq!(metadata.tags, None);
+        assert_eq!(metadata.descriptions, None);
+
+        assert!(metadata.path.exists());
+        remove_file("test-title-empty.toml").unwrap();
+    }
+
+    #[test]
+    fn set_cover() {
+        let path = Path::new("test-title-cover.toml");
+        let mut metadata = TitleMetadata::from(path);
+        metadata.set_cover("test.png".to_string());
+
+        let metadata_file_content = std::fs::read_to_string(&metadata.path).unwrap();
+        assert_eq!(metadata_file_content.contains("cover = \"test.png\""), true);
+
+        assert!(metadata.path.exists());
+        remove_file("test-title-cover.toml").unwrap();
+    }
+
+    #[test]
+    fn path_no_ext() {
+        let path = Path::new("test-title-no-ext");
+        let metadata = TitleMetadata::from(path);
+
+        assert_eq!(metadata.path, Path::new("test-title-no-ext.toml"));
+
+        assert!(metadata.path.exists());
+        remove_file("test-title-no-ext.toml").unwrap();
+    }
+
+    #[test]
+    fn path_overwrite_ext() {
+        let path = Path::new("test-title-overwrite-ext.any");
+        let metadata = TitleMetadata::from(path);
+
+        assert_eq!(metadata.path, Path::new("test-title-overwrite-ext.toml"));
+
+        assert!(metadata.path.exists());
+        remove_file("test-title-overwrite-ext.toml").unwrap();
+    }
+}
+
+#[cfg(test)]
+mod test_category {
+    use super::*;
+    use std::fs::remove_file;
+
+    #[test]
+    fn empty() {
+        let path = Path::new("test-category-empty.toml");
+        let metadata = CategoryMetadata::from(path);
+
+        assert_eq!(metadata.id, None);
+        assert_eq!(metadata.name, None);
+        assert_eq!(metadata.description, None);
+        assert_eq!(metadata.cover, None);
+
+        assert!(metadata.path.exists());
+        remove_file("test-category-empty.toml").unwrap();
+    }
+
+    #[test]
+    fn path_no_ext() {
+        let path = Path::new("test-category-no-ext");
+        let metadata = TitleMetadata::from(path);
+
+        assert_eq!(metadata.path, Path::new("test-category-no-ext.toml"));
+
+        assert!(metadata.path.exists());
+        remove_file("test-category-no-ext.toml").unwrap();
+    }
+
+    #[test]
+    fn path_overwrite_ext() {
+        let path: &Path = Path::new("test-category-overwrite-ext.any");
+        let metadata = TitleMetadata::from(path);
+
+        assert_eq!(metadata.path, Path::new("test-category-overwrite-ext.toml"));
+
+        assert!(metadata.path.exists());
+        remove_file("test-category-overwrite-ext.toml").unwrap();
+    }
+
+    #[test]
+    fn set_id() {
+        let path = Path::new("test-category-set-id.toml");
+        let mut metadata = CategoryMetadata::from(path);
+
+        let category_id = CategoryID::new();
+        metadata.set_id(category_id.clone());
+
+        let metadata_file_content = std::fs::read_to_string(&metadata.path).unwrap();
+
+        assert!(metadata_file_content.contains(format!("id = \"{}\"", category_id).as_str()));
+        remove_file("test-category-set-id.toml").unwrap();
+    }
+}
