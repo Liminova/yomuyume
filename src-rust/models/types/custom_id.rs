27a42c58b3ca951827db8e97f194f2a7046d6ef8
Delnegend <kiennguyen19323@gmail.com>
2024-08-29 17:14:07 +0700

    feat(`custom_id.rs`): mv things around; `impl Nullable` to use `Option<CustomID>`

--- src-rust/models/types/custom_id.rs
+++ src-rust/models/types/custom_id.rs
@@ -16,13 +16,6 @@ impl CustomID {
     }
 
     pub fn from(id: String) -> Result<Self, String> {
-        // for byte in id.as_bytes() {
-        //     match *byte {
-        //         b'0'..=b'9' | b'a'..=b'z' | b'A'..=b'Z' | b'-' | b'_' => (),
-        //         _ => return Err("invalid nanoid string".to_string()),
-        //     }
-        // }
-        // Ok(Self(id))
         id.as_bytes()
             .iter()
             .try_for_each(|byte| match *byte {
@@ -34,10 +27,7 @@ impl CustomID {
 }
 
 impl Serialize for CustomID {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::ser::Serializer,
-    {
+    fn serialize<S: serde::ser::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
         self.0.serialize(serializer)
     }
 }
@@ -50,30 +40,15 @@ impl<'de> serde::de::Visitor<'de> for CustomIDVisitor {
         formatter.write_str("a string only containing alphanumeric characters, - or _")
     }
 
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-    where
-        E: serde::de::Error,
-    {
+    fn visit_str<E: serde::de::Error>(self, value: &str) -> Result<Self::Value, E> {
         CustomID::from(String::from(value))
             .map_err(|_| serde::de::Error::invalid_value(serde::de::Unexpected::Str(value), &self))
     }
 }
 
 impl<'de> Deserialize<'de> for CustomID {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
+    fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
         deserializer.deserialize_string(CustomIDVisitor)
-    }
-}
-
-impl sea_orm::TryFromU64 for CustomID {
-    fn try_from_u64(value: u64) -> Result<Self, sea_orm::DbErr> {
-        match CustomID::from(value.to_string()) {
-            Ok(id) => Ok(id),
-            Err(_) => Err(sea_orm::DbErr::ConvertFromU64("invalid nanoid string")),
-        }
     }
 }
 
@@ -86,12 +61,6 @@ impl From<CustomID> for String {
 impl From<&CustomID> for String {
     fn from(id: &CustomID) -> Self {
         id.0.clone()
-    }
-}
-
-impl From<&CustomID> for sea_orm::Value {
-    fn from(id: &CustomID) -> Self {
-        sea_orm::Value::String(Some(Box::new(id.0.clone())))
     }
 }
 
@@ -110,5 +79,31 @@ impl std::fmt::Display for CustomID {
 impl AsRef<str> for CustomID {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+// SeaORM New Type - https://www.sea-ql.org/SeaORM/docs/generate-entity/newtype/
+// 1, 2, 3. satisfied by the DeriveValueType macro
+// 4. If the field is Option<T>, implement sea_query::Nullable for T
+impl sea_orm::sea_query::Nullable for CustomID {
+    fn null() -> sea_orm::Value {
+        sea_orm::Value::String(None)
+    }
+}
+
+// SeaORM wouldn't shut up about this even though we don't use auto increment
+impl sea_orm::TryFromU64 for CustomID {
+    fn try_from_u64(value: u64) -> Result<Self, sea_orm::DbErr> {
+        match Self::from(value.to_string()) {
+            Ok(id) => Ok(id),
+            Err(e) => Err(sea_orm::DbErr::Custom(e)),
+        }
+    }
+}
+
+// to be able to use references when writing conditions, instead of cloning
+impl From<&CustomID> for sea_orm::Value {
+    fn from(id: &CustomID) -> Self {
+        sea_orm::Value::String(Some(Box::new(id.0.clone())))
     }
 }
