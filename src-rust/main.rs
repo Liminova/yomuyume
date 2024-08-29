17de2f283077243c7b26f15ab6f107cf5e481767
Delnegend <kiennguyen19323@gmail.com>
2024-08-29 17:34:07 +0700

    feat(`main.rs`): `impl Display for AppError`

--- src-rust/main.rs
+++ src-rust/main.rs
@@ -50,6 +50,12 @@ impl IntoResponse for AppError {
     }
 }
 
+impl Display for AppError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
 // This enables using `?` on functions that return `Result<_, anyhow::Error>` to turn them into
 // `Result<_, AppError>`. That way you don't need to do that manually.
 impl<E> From<E> for AppError
