731655243fbe2acc46fe801c9d1c040bdf9d6ddc
Delnegend <kiennguyen19323@gmail.com>
2024-08-31 15:03:43 +0700

    refactor(`models`): mv `prelude` inside `mod.rs`

--- src-rust/models/mod.rs
+++ src-rust/models/mod.rs
@@ -10,5 +10,29 @@ pub mod titles;
 pub mod titles_tags;
 pub mod users;
 
-pub mod prelude;
 pub mod types;
+
+pub mod prelude {
+    pub use super::bookmarks::Entity as Bookmarks;
+    pub use super::categories::Entity as Categories;
+    pub use super::code_delete_account::Entity as CodeDeleteAccount;
+    pub use super::code_reset_password::Entity as CodeResetPassword;
+    pub use super::code_validate_email::Entity as CodeValidateEmail;
+    pub use super::favorites::Entity as Favorites;
+    pub use super::pages::Entity as Pages;
+    pub use super::progresses::Entity as Progresses;
+    pub use super::session_tokens::Entity as SessionTokens;
+    pub use super::tags::Entity as Tags;
+    pub use super::titles::Entity as Titles;
+    pub use super::titles_tags::Entity as TitlesTags;
+    pub use super::users::Entity as Users;
+
+    pub use super::categories::CategoryID;
+    pub use super::pages::PageID;
+    pub use super::titles::TitleID;
+    pub use super::users::UserID;
+
+    pub use super::types::custom_id::CustomID;
+
+    pub use super::*;
+}
