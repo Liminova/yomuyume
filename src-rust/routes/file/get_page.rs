522bd26346406e156b40650c7df7ba50b12820ae
Delnegend <kiennguyen19323@gmail.com>
2024-08-31 16:55:56 +0700

    refactor(`routes`): rework `get_page.rs`
    - extract TitleId and Path from Pages
    - lowercase errors
    - chaining methods for extract page from content file

--- src-rust/routes/file/get_page.rs
+++ src-rust/routes/file/get_page.rs
@@ -7,7 +7,7 @@ use axum::{
     http::{header, StatusCode},
     response::{IntoResponse, Response},
 };
-use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
+use sea_orm::{ColumnTrait, EntityTrait, QueryFilter, QuerySelect};
 use zip::ZipArchive;
 
 #[utoipa::path(get, path = "/api/file/page/{page_id}", responses(
@@ -20,38 +20,39 @@ pub async fn get_page(
     State(data): State<Arc<AppState>>,
     Path(page_id): Path<String>,
 ) -> Result<Response, AppError> {
-    let page_in_db = match Pages::find()
+    let (title_id, path_in_content_file) = match Pages::find()
+        .select_only()
+        .columns(vec![pages::Column::TitleId, pages::Column::Path])
         .filter(pages::Column::Id.contains(page_id))
+        .into_tuple::<(String, String)>()
         .one(&data.db)
         .await
-        .map_err(|e| AppError::from(anyhow::anyhow!("Can't find page: {}", e)))?
+        .map_err(|e| AppError::from(anyhow::anyhow!("can't find page: {}", e)))?
     {
         Some(page) => page,
-        None => return Ok((StatusCode::NOT_FOUND, "Page not found.".to_string()).into_response()),
+        None => return Ok((StatusCode::NOT_FOUND, "page not found".to_string()).into_response()),
     };
 
     let title_in_db = match Titles::find()
-        .filter(titles::Column::Id.contains(&page_in_db.title_id))
+        .filter(titles::Column::Id.contains(&title_id))
         .one(&data.db)
         .await
-        .map_err(|e| AppError::from(anyhow::anyhow!("Can't find title: {}", e)))?
+        .map_err(|e| AppError::from(anyhow::anyhow!("can't find title: {}", e)))?
     {
         Some(title) => title,
-        None => return Ok((StatusCode::NOT_FOUND, "Title not found.".to_string()).into_response()),
+        None => return Ok((StatusCode::NOT_FOUND, "title not found".to_string()).into_response()),
     };
 
-    // zip file -> page file -> buffer
-    let mut zip = ZipArchive::new(
-        File::open(title_in_db.path)
-            .map_err(|e| AppError::from(anyhow::anyhow!("Open zip file error: {}", e)))?,
-    )
-    .map_err(|e| AppError::from(anyhow::anyhow!("Read zip file error: {}", e)))?;
-    let mut file = zip
-        .by_name(page_in_db.path.as_ref())
-        .map_err(|e| AppError::from(anyhow::anyhow!("Get page file error: {}", e)))?;
     let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer)
-        .map_err(|e| AppError::from(anyhow::anyhow!("Read page file error: {}", e)))?;
+    ZipArchive::new(
+        File::open(title_in_db.path)
+            .map_err(|e| AppError::from(anyhow::anyhow!("open zip file error: {}", e)))?,
+    )
+    .map_err(|e| AppError::from(anyhow::anyhow!("read zip file error: {}", e)))?
+    .by_name(&path_in_content_file)
+    .map_err(|e| AppError::from(anyhow::anyhow!("get page file error: {}", e)))?
+    .read_to_end(&mut buffer)
+    .map_err(|e| AppError::from(anyhow::anyhow!("read page file error: {}", e)))?;
 
     Ok((
         StatusCode::OK,
@@ -59,7 +60,7 @@ pub async fn get_page(
             header::CONTENT_TYPE,
             format!(
                 "image/{}",
-                PathBuf::from(page_in_db.path)
+                PathBuf::from(path_in_content_file)
                     .extension()
                     .map(|s| s.to_str().unwrap_or(""))
                     .unwrap_or("")
