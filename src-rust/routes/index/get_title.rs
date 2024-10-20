d721deaab5f5ad2945b1053627e13031e0837b1b
Delnegend <kiennguyen19323@gmail.com>
2024-10-21 06:27:40 +0700

    style(`routes/index/get_titles`): add space in response struct

--- src-rust/routes/index/get_title.rs
+++ src-rust/routes/index/get_title.rs
@@ -29,6 +29,7 @@ pub struct TitleResponseBody {
     pub author: Option<String>,
     pub description: Option<String>,
     pub release_date: Option<String>,
+
     pub cover_blurhash: Option<String>,
     pub blurhash_width: Option<u8>,
     pub blurhash_height: Option<u8>,
