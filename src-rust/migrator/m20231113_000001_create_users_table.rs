d26b0897b1c17cb2583928d02765a5b6f6736e3f
Delnegend <kiennguyen19323@gmail.com>
2024-10-20 22:28:36 +0700

    feat(`migrator/users`): `IsVerified` to `VerifiedAt`

--- src-rust/migrator/m20231113_000001_create_users_table.rs
+++ src-rust/migrator/m20231113_000001_create_users_table.rs
@@ -22,10 +22,10 @@ impl MigrationTrait for Migration {
                     .col(string_uniq(Users::Email))
                     .col(string_null(Users::ProfilePicture))
                     .col(date_time(Users::CreatedAt))
-                    .col(boolean(Users::IsVerified))
                     .col(string_null(Users::IpAddress))
                     .col(date_time_null(Users::UpdatedAt))
                     .col(string(Users::PasswordHash))
+                    .col(date_time_null(Users::VerifiedAt))
                     .to_owned(),
             )
             .await
@@ -48,6 +48,6 @@ pub enum Users {
     CreatedAt,
     IpAddress,
     UpdatedAt,
-    IsVerified,
     PasswordHash,
+    VerifiedAt,
 }
