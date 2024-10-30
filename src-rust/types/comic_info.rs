720c4ae67d343b72073f5ab71c7f1c8ee9d3efdf
Delnegend <kiennguyen19323@gmail.com>
2024-10-30 13:57:06 +0700

    refactor(`types`): mv `option_blurhash_deserializer`, `option_datetime_deserializer` to `mod.rs`

--- src-rust/types/comic_info.rs
+++ src-rust/types/comic_info.rs
@@ -12,6 +12,7 @@ use murmur3::murmur3_32;
 use quick_xml::de::from_str;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
+use super::{option_blurhash_deserializer, option_datetime_deserializer};
 use crate::COMICINFO_SCHEMA;
 
 #[derive(Debug, Clone, Default, PartialEq, Eq, Deserialize, Serialize)]
@@ -698,41 +699,6 @@ impl ComicInfo {
             .context("can't serialize ComicInfo to pretty string")?;
         Ok(buffer)
     }
-}
-
-fn option_blurhash_deserializer<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?.trim().to_string();
-    if s.is_empty() {
-        return Ok(None);
-    }
-    if let Err(e) = blurhash::decode(&s, 0, 0, 0.0) {
-        return Err(serde::de::Error::custom(e));
-    }
-    Ok(Some(s.to_string()))
-}
-
-fn option_datetime_deserializer<'de, D>(deserializer: D) -> Result<Option<DateTime<Utc>>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?.trim().to_string();
-    if s.is_empty() {
-        return Ok(None);
-    }
-
-    let native_datetime = s
-        .parse::<NaiveDateTime>()
-        .map_err(|e| serde::de::Error::custom(format!("can't parse as NaiveDateTime: {e}")))?;
-
-    let local_datetime = Local
-        .from_local_datetime(&native_datetime)
-        .single()
-        .ok_or_else(|| serde::de::Error::custom("invalid datetime"))?;
-
-    Ok(Some(local_datetime.to_utc()))
 }
 
 #[cfg(test)]
