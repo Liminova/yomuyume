0c1ff611fd251288f7f1da0f5708d55a857a5c76
Delnegend <kiennguyen19323@gmail.com>
2024-11-01 20:26:57 +0700

    refactor(`app_state`): replace `sea_orm::DatabaseConnection` w/ `sqlx::PgPool`

--- src-rust/utils/app_state.rs
+++ src-rust/utils/app_state.rs
@@ -1,12 +1,10 @@
 use tokio::sync::Mutex;
 
-use sea_orm::DatabaseConnection;
-
 use crate::utils::config::Config;
 
 #[derive(Debug)]
 pub struct AppState {
-    pub db: DatabaseConnection,
+    pub pool: sqlx::PgPool,
     pub config: Config,
     pub scanning_complete: Mutex<bool>,
     pub scanning_progress: Mutex<f64>,
