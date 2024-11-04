6364e7e8022864471eab211b0b3a80906ba38a9c
Delnegend <kiennguyen19323@gmail.com>
2024-11-05 00:28:34 +0700

    refactor(`types/mod`): rm `option_datetime_deserializer`

--- src-rust/types/mod.rs
+++ src-rust/types/mod.rs
@@ -3,29 +3,7 @@ pub mod comic_info;
 pub mod custom_id;
 pub mod temp_code_purpose;
 
-use chrono::{DateTime, Local, NaiveDateTime, TimeZone, Utc};
 use serde::{Deserialize, Deserializer};
-
-fn option_datetime_deserializer<'de, D>(deserializer: D) -> Result<Option<DateTime<Utc>>, D::Error>
-where
-    D: Deserializer<'de>,
-{
-    let s = String::deserialize(deserializer)?.trim().to_string();
-    if s.is_empty() {
-        return Ok(None);
-    }
-
-    let native_datetime = s
-        .parse::<NaiveDateTime>()
-        .map_err(|e| serde::de::Error::custom(format!("can't parse as NaiveDateTime: {e}")))?;
-
-    let local_datetime = Local
-        .from_local_datetime(&native_datetime)
-        .single()
-        .ok_or_else(|| serde::de::Error::custom("invalid datetime"))?;
-
-    Ok(Some(local_datetime.to_utc()))
-}
 
 fn option_blurhash_deserializer<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
 where
