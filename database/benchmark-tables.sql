7ec3cd32aab78916c04f3ccbdbb39adf0f455836
Delnegend <kiennguyen19323@gmail.com>
2024-11-05 22:50:26 +0700

    fix(`benchmark-tables.sql`): missing comma

--- database/benchmark-tables.sql
+++ database/benchmark-tables.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS pages_benchmark (
     id TEXT PRIMARY KEY,
     title_id TEXT NOT NULL,
     path VARCHAR NOT NULL,
-    description VARCHAR
+    description VARCHAR,
 
     CONSTRAINT "uc-pages_benchmark-title_id-path" UNIQUE (title_id, path)
 );
