4a3ea98d90891469e86aac918184bb260de95c88
Delnegend <kiennguyen19323@gmail.com>
2024-11-05 22:33:03 +0700

    chore(`benches`): rm irrelevant benchmarks

--- benches/main.rs
+++ benches/main.rs
@@ -1,12 +1,5 @@
 mod batch_upsert;
-mod mut_or_map_unzip;
-mod nanoid_gen;
 use criterion::{criterion_group, criterion_main};
 
-criterion_group!(
-    benches,
-    batch_upsert::main,
-    nanoid_gen::main,
-    mut_or_map_unzip::main
-);
+criterion_group!(benches, batch_upsert::main);
 criterion_main!(benches);
