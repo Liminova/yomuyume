7882462157e3e2a6dd4ed4f6408349f93731d911
Delnegend <kiennguyen19323@gmail.com>
2024-11-07 20:57:00 +0700

    refactor(`upsert_category`): takes in `title_path` directly instead of `category_dir_path`
    - custom error type
    - inline tracing
    - rename `category_dir_path_string` to `category_path_string`

--- src-rust/library_processor/upsert_category.rs
+++ src-rust/library_processor/upsert_category.rs
@@ -1,9 +1,12 @@
-use std::{path::Path, sync::Arc};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 
-use anyhow::{anyhow, Context, Result};
+use anyhow::{Context, Result};
 use chrono::{DateTime, Utc};
-use tracing::warn;
 
+use super::upsert_title::COMICINFO_FILENAME;
 use crate::{
     library_processor::blurhash::encode,
     types::{category_info::CategoryInfo, CategoryID},
@@ -12,28 +15,53 @@ use crate::{
 
 const CATEGORY_INFO_FILENAME: &str = "CategoryInfo.xml";
 
-/// Upsert a category to the database and return its ID.
-pub async fn upsert_category(
+pub(super) enum UpsertCategoryError {
+    DirIsInLibraryRoot,
+    DirIsTitle,
+    Other(anyhow::Error),
+}
+
+impl From<anyhow::Error> for UpsertCategoryError {
+    fn from(e: anyhow::Error) -> Self {
+        Self::Other(e)
+    }
+}
+
+/// Decide if a given title path is in any subdirectory of the library path,
+/// and if so, upsert the category to the database and return its ID.
+pub async fn upsert_category<'e>(
     app_state: Arc<AppState>,
-    category_dir_path: &Path,
-) -> Result<CategoryID> {
-    '_pre_checks: {
-        if !category_dir_path.exists() {
-            return Err(anyhow!("category dir not found"));
-        }
-        if !category_dir_path.is_dir() {
-            return Err(anyhow!("category dir is not a directory"));
-        }
+    title_path: &Path,
+    conn: impl sqlx::Executor<'e, Database = sqlx::Postgres> + 'e,
+) -> Result<CategoryID, UpsertCategoryError> {
+    let category_path = match title_path
+        .parent()
+        .filter(|p| {
+            p.strip_prefix(app_state.config.library_path.clone())
+                .map(|p| !p.to_string_lossy().to_string().is_empty())
+                .unwrap_or(false)
+        })
+        .map(PathBuf::from)
+    {
+        Some(category_path) => category_path,
+        None => return Err(UpsertCategoryError::DirIsInLibraryRoot),
+    };
+
+    // TODO: support directory-as-title
+    if category_path.join(COMICINFO_FILENAME).exists() {
+        return Err(UpsertCategoryError::DirIsTitle);
     }
 
-    let category_dir_path_string = category_dir_path.to_string_lossy().to_string();
+    let category_path_string = category_path.to_string_lossy().to_string();
 
-    let category_info_path = category_dir_path.join(CATEGORY_INFO_FILENAME);
+    let category_info_path = category_path.join(CATEGORY_INFO_FILENAME);
     let mut category_info = CategoryInfo::from_str(
         &std::fs::read_to_string(&category_info_path)
-            .context(format!("can't read {CATEGORY_INFO_FILENAME}"))?,
+            .context(format!("can't read {CATEGORY_INFO_FILENAME}"))
+            .map_err(UpsertCategoryError::Other)?,
     )
-    .context(format!("can't parse {CATEGORY_INFO_FILENAME}"))?;
+    .context(format!("can't parse {CATEGORY_INFO_FILENAME}"))
+    .map_err(UpsertCategoryError::Other)?;
 
     let (cover_path, cover_blurhash, cover_width, cover_height) = 'scoped: {
         let cover = match category_info.cover.as_mut() {
@@ -55,7 +83,9 @@ pub async fn upsert_category(
             })
             .map(|d| d.into());
         if let Err(ref e) = real_modified_date {
-            warn!("can't get modified date of cover file for {category_dir_path_string}: {e:#}");
+            tracing::warn!(
+                "can't get modified date of cover file for {category_path_string}: {e:#?}"
+            );
         }
 
         if let (Some(blurhash), Some(modified_date_at_encode), Ok(real_modified_date)) = (
@@ -95,7 +125,9 @@ pub async fn upsert_category(
                 );
             }
             Err(e) => {
-                warn!("can't encode the configured cover of {category_dir_path_string}: {e:#}");
+                tracing::warn!(
+                    "can't encode the configured cover of {category_path_string}: {e:#?}"
+                );
             }
         }
 
@@ -103,8 +135,14 @@ pub async fn upsert_category(
     };
 
     '_save_category_info: {
-        std::fs::write(&category_info_path, category_info.to_pretty_string()?)
-            .context(format!("can't write to {CATEGORY_INFO_FILENAME}"))?;
+        std::fs::write(
+            &category_info_path,
+            category_info
+                .to_pretty_string()
+                .map_err(UpsertCategoryError::Other)?,
+        )
+        .context(format!("can't write to {CATEGORY_INFO_FILENAME}"))
+        .map_err(UpsertCategoryError::Other)?;
     }
 
     let category_id = sqlx::query!(
@@ -127,18 +165,20 @@ pub async fn upsert_category(
             .id_generator
             .snowflake()
             .await
-            .context("can't generate category id")?,
+            .context("can't generate category id")
+            .map_err(UpsertCategoryError::Other)?,
         category_info.name.clone().unwrap_or("Untitled".to_string()),
         category_info.description.as_ref(),
-        &category_dir_path_string,
+        &category_path_string,
         cover_path,
         cover_blurhash,
         cover_width,
         cover_height
     )
-    .fetch_one(&app_state.pool)
+    .fetch_one(conn)
     .await
-    .context("can't upsert category to database")?
+    .context("can't upsert category to database")
+    .map_err(UpsertCategoryError::Other)?
     .id;
 
     Ok(category_id)
