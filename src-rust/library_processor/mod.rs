e53df2d1866def2983cc31cac1f45397cd979872
Delnegend <kiennguyen19323@gmail.com>
2024-11-07 22:01:14 +0700

    feat(`library_processor`): replace old `Scanner` w/ `LibraryProcessor`; parallel processing; better errors

--- src-rust/library_processor/mod.rs
+++ src-rust/library_processor/mod.rs
@@ -3,22 +3,21 @@ mod upsert_category;
 mod upsert_title;
 
 use std::{
-    collections::HashMap,
+    mem::drop,
     path::{Path, PathBuf},
     sync::Arc,
 };
 
 use anyhow::{Context, Result};
-use tracing::debug;
+use futures_util::future::join_all;
+use tokio::sync::Semaphore;
+use tracing::{error, info};
 
-use crate::{types::CategoryID, AppState, SUPPORTED_ARCHIVE_FORMATS};
-use upsert_category::upsert_category;
-use upsert_title::upsert_title;
-
-#[derive(Debug)]
-pub struct Scanner {
-    app_state: Arc<AppState>,
-}
+use crate::{
+    types::{CategoryID, TitleID},
+    AppState, SUPPORTED_ARCHIVE_FORMATS,
+};
+use upsert_title::{upsert_title, UpsertTitleError, UpsertTitleOk};
 
 async fn read_dir_iterative(path: &Path) -> Result<Vec<PathBuf>> {
     let mut files: Vec<PathBuf> = Vec::new();
@@ -42,77 +41,128 @@ async fn read_dir_iterative(path: &Path) -> Result<Vec<PathBuf>> {
     Ok(files)
 }
 
-impl Scanner {
-    pub async fn new(app_state: Arc<AppState>) -> Self {
+#[derive(Debug)]
+pub struct LibraryProcessor {
+    app_state: Arc<AppState>,
+    sem: Arc<Semaphore>,
+}
+
+#[derive(Debug)]
+pub enum ProcessTitleError {
+    CantAcquirePermit((anyhow::Error, PathBuf)),
+    CantSpawnTask((anyhow::Error, PathBuf)),
+    Other((UpsertTitleError, PathBuf)),
+}
+
+type ProcessTitleOk = UpsertTitleOk;
+
+impl LibraryProcessor {
+    pub fn new(app_state: Arc<AppState>) -> Self {
         Self {
-            app_state: Arc::clone(&app_state),
+            app_state,
+            sem: Arc::new(Semaphore::new(num_cpus::get())),
         }
     }
 
-    pub async fn run(&self) -> Result<()> {
-        let files_in_lib = read_dir_iterative(&self.app_state.config.library_path).await?;
-        debug!("found {} files in library", files_in_lib.len());
+    pub async fn process_title(
+        &self,
+        title_path: PathBuf,
+    ) -> Result<ProcessTitleOk, ProcessTitleError> {
+        let app_state_clone = self.app_state.clone();
+        let sem_clone = self.sem.clone();
+        let title_path_clone = title_path.clone();
 
-        let mut category_path_id_map: HashMap<PathBuf, CategoryID> = HashMap::new();
+        let task = tokio::spawn(async move {
+            let permit = sem_clone
+                .acquire()
+                .await
+                .context("can't acquire a permit from semaphore")
+                .map_err(|e| ProcessTitleError::CantAcquirePermit((e, title_path_clone.clone())))?;
 
-        for title_path in files_in_lib {
-            if !SUPPORTED_ARCHIVE_FORMATS.contains(
-                &title_path
-                    .extension()
-                    .unwrap_or_default()
-                    .to_str()
-                    .unwrap_or_default(),
-            ) {
-                continue;
-            }
+            let res = upsert_title(app_state_clone, &title_path_clone.clone())
+                .await
+                .map_err(|e| ProcessTitleError::Other((e, title_path_clone.clone())));
 
-            // title inside a subdir -> in a category
-            // title inside library root -> no category
-            let category_path = title_path
-                .parent()
-                .filter(|p| {
-                    p.strip_prefix(&self.app_state.config.library_path)
-                        .map(|p| !p.to_string_lossy().to_string().is_empty())
-                        .unwrap_or(false)
+            drop(permit);
+
+            res
+        });
+
+        match task.await {
+            Ok(task) => task,
+            Err(e) => Err(ProcessTitleError::CantSpawnTask((e.into(), title_path))),
+        }
+    }
+
+    pub async fn full_scan(&self) {
+        let files_in_lib = match read_dir_iterative(&self.app_state.config.library_path).await {
+            Ok(files) => files
+                .into_iter()
+                .filter(|title_path| {
+                    SUPPORTED_ARCHIVE_FORMATS.contains(
+                        &title_path
+                            .extension()
+                            .unwrap_or_default()
+                            .to_str()
+                            .unwrap_or_default(),
+                    )
                 })
-                .map(PathBuf::from);
+                .collect::<Vec<_>>(),
+            Err(e) => {
+                error!("can't scan library: {e:#?}");
+                return;
+            }
+        };
+        info!("found {} files in library", files_in_lib.len());
 
-            if let Some(category_path) = category_path {
-                let category_id = *category_path_id_map.entry(category_path.clone()).or_insert(
-                    upsert_category(self.app_state.clone(), &category_path)
-                        .await
-                        .context(format!(
-                            "can't insert category to database: {}",
-                            category_path.display()
-                        ))?,
-                );
+        let upsert_title_results = files_in_lib
+            .into_iter()
+            .map(|title_path| self.process_title(title_path));
 
-                upsert_title(self.app_state.clone(), Some(category_id), &title_path)
-                    .await
-                    .context(format!(
-                        "can't insert title to database: {}",
-                        title_path.display()
-                    ))?;
-            } else {
-                upsert_title(self.app_state.clone(), None, &title_path)
-                    .await
-                    .context(format!(
-                        "can't insert title to database: {}",
-                        title_path.display()
-                    ))?;
+        let mut category_ids: Vec<CategoryID> = Vec::with_capacity(upsert_title_results.len());
+        let mut title_ids: Vec<TitleID> = Vec::with_capacity(upsert_title_results.len());
+
+        for result in join_all(upsert_title_results).await {
+            match result {
+                Ok(result) => {
+                    title_ids.push(result.title_id);
+                    if let Some(category_id) = result.category_id {
+                        category_ids.push(category_id);
+                    }
+                }
+                Err(e) => match e {
+                    ProcessTitleError::CantAcquirePermit((e, t)) => {
+                        error!("can't acquire a permit to process title {t:?}: {e:#?}")
+                    }
+                    ProcessTitleError::CantSpawnTask((e, t)) => {
+                        error!("can't spawn task to process title {t:?}: {e:#?}")
+                    }
+                    ProcessTitleError::Other((e, t)) => match e {
+                        UpsertTitleError::IsIgnored => info!("title {t:?} is ignored"),
+                        UpsertTitleError::Other(error) => {
+                            error!("can't process title: {error:#?}")
+                        }
+                    },
+                },
             }
         }
 
-        sqlx::query!(
-            "DELETE FROM categories WHERE id NOT IN (SELECT id FROM UNNEST($1::bigint[]))",
-            &category_path_id_map.values().cloned().collect::<Vec<_>>()
+        if let Err(e) = sqlx::query!(
+            r#"WITH _ AS (
+                DELETE FROM categories
+                    WHERE id NOT IN (SELECT id FROM UNNEST($1::bigint[]))
+            )
+            DELETE FROM titles
+                WHERE id NOT IN (SELECT id FROM UNNEST($2::bigint[]))"#,
+            &category_ids,
+            &title_ids,
         )
         .execute(&self.app_state.pool)
         .await
-        .context("can't delete categories")?;
+        {
+            error!("can't cleanup non-exist categories and titles from database: {e:#?}");
+        };
 
-        tracing::info!("scanner finished");
-
-        Ok(())
+        info!("finished processing library");
     }
 }
