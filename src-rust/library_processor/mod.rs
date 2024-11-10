88359a504fd372b8c1b1ad4a0e93e3e391f9cb5e
Delnegend <kiennguyen19323@gmail.com>
2024-11-10 23:13:36 +0700

    feat(`library_processor/mod`): rm unecessary structs and type; new one fn `full_scan()` w/ completed logics

--- src-rust/library_processor/mod.rs
+++ src-rust/library_processor/mod.rs
@@ -2,181 +2,197 @@ mod blurhash;
 mod dir_entry_guesser;
 mod upsert_category;
 mod upsert_oneshot;
+mod upsert_series;
 
 use std::{
-    collections::VecDeque,
-    fs::{self, DirEntry},
+    collections::{HashMap, VecDeque},
+    fs::DirEntry,
     mem::drop,
-    path::{Path, PathBuf},
+    path::PathBuf,
     sync::Arc,
 };
 
 use anyhow::{Context, Result};
 use dir_entry_guesser::{DirEntryType, DirEntryTypeGuesser};
 use futures_util::future::join_all;
-use tokio::sync::Semaphore;
+use tokio::sync::{Mutex, Semaphore};
 use tracing::{debug, error, info};
 
-use crate::{
-    types::{CategoryID, TitleID},
-    AppState, SUPPORTED_ARCHIVE_FORMATS,
-};
-use upsert_oneshot::{upsert_title, UpsertTitleError, UpsertTitleOk};
+use crate::AppState;
+use upsert_oneshot::{upsert_oneshot, OneshotType};
 
 #[derive(Debug)]
-pub struct LibraryProcessor {
-    app_state: Arc<AppState>,
-    sem: Arc<Semaphore>,
-}
-
-#[derive(Debug)]
-pub enum ProcessTitleError {
-    CantAcquirePermit((anyhow::Error, PathBuf)),
-    CantSpawnTask((anyhow::Error, PathBuf)),
-    Other((UpsertTitleError, PathBuf)),
-}
-
-type ProcessTitleOk = UpsertTitleOk;
-
 struct ScannedEntry {
     entry: DirEntry,
-    parent: Option<PathBuf>,
+    parent: Option<PathBuf>, // None for the library root
 }
 
-impl LibraryProcessor {
-    pub fn new(app_state: Arc<AppState>) -> Self {
-        Self {
-            app_state,
-            sem: Arc::new(Semaphore::new(num_cpus::get())),
-        }
+pub async fn full_scan(app_state: Arc<AppState>) -> Result<()> {
+    // scan library directory
+    let library_dir = std::fs::read_dir(&app_state.config.library_path).context(format!(
+        "can't read library path: {:?}",
+        app_state.config.library_path
+    ))?;
+
+    let mut queue: VecDeque<ScannedEntry> = VecDeque::new();
+
+    // populate first layer of the library tree to queue
+    for entry in library_dir {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(e) => {
+                error!("can't extract entry from root library: {e:?}",);
+                continue;
+            }
+        };
+        queue.push_front(ScannedEntry {
+            entry,
+            parent: None,
+        });
     }
 
-    // pub async fn process_title(
-    //     &self,
-    //     title_path: PathBuf,
-    // ) -> Result<ProcessTitleOk, ProcessTitleError> {
-    //     let app_state_clone = self.app_state.clone();
-    //     let sem_clone = self.sem.clone();
-    //     let title_path_clone = title_path.clone();
+    // extract necessary values, avoid read lock
+    let (nomedia_support, komga_oneshot_support, komga_recycle_support) = {
+        let live_config = app_state.live_config.read().await;
+        (
+            live_config.nomedia_support,
+            live_config.komga_oneshot_support,
+            live_config.komga_recycle_support,
+        )
+    };
 
-    //     let task = tokio::spawn(async move {
-    //         let permit = sem_clone
-    //             .acquire()
-    //             .await
-    //             .context("can't acquire a permit from semaphore")
-    //             .map_err(|e| ProcessTitleError::CantAcquirePermit((e, title_path_clone.clone())))?;
+    // processing tasks waiting to be .await-ed
+    let mut tasks = vec![];
+    let category_path_to_id: Arc<Mutex<HashMap<PathBuf, i64>>> =
+        Arc::new(Mutex::new(HashMap::new()));
 
-    //         let res = upsert_title(app_state_clone, &title_path_clone.clone())
-    //             .await
-    //             .map_err(|e| ProcessTitleError::Other((e, title_path_clone.clone())));
-
-    //         drop(permit);
-
-    //         res
-    //     });
-
-    //     match task.await {
-    //         Ok(task) => task,
-    //         Err(e) => Err(ProcessTitleError::CantSpawnTask((e.into(), title_path))),
-    //     }
-    // }
-
-    pub async fn full_scan(&self) -> Result<()> {
-        // scan library directory
-        let library_dir =
-            std::fs::read_dir(&self.app_state.config.library_path).context(format!(
-                "can't read library path: {:?}",
-                self.app_state.config.library_path
-            ))?;
-
-        let mut queue: VecDeque<ScannedEntry> = VecDeque::new();
-
-        // populate first layer of the library tree to queue
-        for entry in library_dir {
-            let entry = match entry {
-                Ok(entry) => entry,
-                Err(e) => {
-                    error!("can't extract entry from root library: {e:#?}",);
-                    continue;
-                }
-            };
-            queue.push_front(ScannedEntry {
-                entry,
-                parent: None,
-            });
-        }
-
-        // extract necessary values, avoid read lock
-        let (nomedia_support, komga_oneshot_support, komga_recycle_support) = {
-            let live_config = self.app_state.live_config.read().await;
-            (
-                live_config.nomedia_support,
-                live_config.komga_oneshot_support,
-                live_config.komga_recycle_support,
-            )
-        };
-
-        // processing tasks waiting to be .await-ed
-        // let mut food_processor = vec![];
-
-        // BFS
-        while let Some(scanned) = queue.pop_back() {
-            let entry_path = scanned.entry.path();
-            match scanned.entry.guess(
-                nomedia_support,
-                komga_oneshot_support,
-                komga_recycle_support,
-            ) {
-                Ok(entry_type) => match entry_type {
-                    DirEntryType::CategoryDir(sub_entries) => {
-                        sub_entries.into_iter().for_each(|sub_entry| {
-                            queue.push_front(ScannedEntry {
-                                entry: sub_entry,
-                                parent: Some(entry_path.clone()),
-                            });
+    // BFS
+    while let Some(scanned) = queue.pop_back() {
+        let entry_path = scanned.entry.path();
+        match scanned.entry.guess(
+            nomedia_support,
+            komga_oneshot_support,
+            komga_recycle_support,
+        ) {
+            Ok(entry_type) => match entry_type {
+                DirEntryType::CategoryDir(sub_entries) => {
+                    sub_entries.into_iter().for_each(|sub_entry| {
+                        queue.push_front(ScannedEntry {
+                            entry: sub_entry,
+                            parent: Some(entry_path.clone()),
                         });
-                    }
-                    DirEntryType::SeriesDir(chapter_path_and_number) => {
-                        println!("TODO: handle series w/ {chapter_path_and_number:?}")
-                    }
-                    DirEntryType::OneShotDir(pages) => {
-                        println!("TODO: handle one-shot w/ {pages:?}")
-                    }
-                    DirEntryType::OneShotArchiveFile => {
-                        println!("TODO: handle one-shot archive file")
-                    }
-                    DirEntryType::Ignored => {
-                        debug!("ignored directory {}", entry_path.display())
-                    }
-                },
-                Err(e) => {
-                    error!(
-                        "can't guess the directory type for {}: {e:#?}",
-                        entry_path.display()
-                    );
-                    continue;
+                    });
                 }
+                DirEntryType::SeriesDir(chapter_path_and_number) => {
+                    println!("TODO: handle series w/ {chapter_path_and_number:?}")
+                }
+                DirEntryType::OneShotDir(pages) => {
+                    tasks.push((
+                        upsert_oneshot(
+                            app_state.clone(),
+                            entry_path.clone(),
+                            OneshotType::InDirectory(pages),
+                            scanned.parent,
+                            category_path_to_id.clone(),
+                            nomedia_support,
+                        ),
+                        entry_path,
+                    ));
+                }
+                DirEntryType::OneShotArchiveFile(archive_file) => {
+                    tasks.push((
+                        upsert_oneshot(
+                            app_state.clone(),
+                            entry_path.clone(),
+                            OneshotType::InArchive(archive_file),
+                            scanned.parent,
+                            category_path_to_id.clone(),
+                            nomedia_support,
+                        ),
+                        entry_path,
+                    ));
+                }
+                DirEntryType::Ignored => {
+                    debug!("ignored directory {}", entry_path.display())
+                }
+            },
+            Err(e) => {
+                error!(
+                    "can't guess the directory type for {}: {e:?}",
+                    entry_path.display()
+                );
+                continue;
             }
         }
-
-        Ok(())
-
-        // if let Err(e) = sqlx::query!(
-        //     r#"WITH _ AS (
-        //         DELETE FROM categories
-        //             WHERE id NOT IN (SELECT id FROM UNNEST($1::bigint[]))
-        //     )
-        //     DELETE FROM titles
-        //         WHERE id NOT IN (SELECT id FROM UNNEST($2::bigint[]))"#,
-        //     &category_ids,
-        //     &title_ids,
-        // )
-        // .execute(&self.app_state.pool)
-        // .await
-        // {
-        //     error!("can't cleanup non-exist categories and titles from database: {e:#?}");
-        // };
-
-        // info!("finished processing library");
     }
+
+    let sem = Arc::new(Semaphore::new(num_cpus::get()));
+    let mut futs = vec![];
+    let upserted_title_ids = Arc::new(Mutex::new(vec![]));
+
+    for (task, title_path) in tasks {
+        let sem = sem.clone();
+        let upserted_title_ids = upserted_title_ids.clone();
+        futs.push(tokio::spawn(async move {
+            let title_path = title_path.display();
+
+            let permit = match sem.acquire().await {
+                Ok(permit) => permit,
+                Err(e) => {
+                    error!(
+                        "can't process {title_path}: can't acquire a permit from semaphore: {:?}",
+                        e
+                    );
+                    return;
+                }
+            };
+
+            match task.await {
+                Ok(upserted_title_id) => upserted_title_ids.lock().await.push(upserted_title_id),
+                Err(e) => match e {
+                    upsert_oneshot::UpsertOneshotErr::IsIgnored => {
+                        info!("title {title_path} is ignored")
+                    }
+                    upsert_oneshot::UpsertOneshotErr::IsEmpty => {
+                        info!("title {title_path} is empty")
+                    }
+                    upsert_oneshot::UpsertOneshotErr::Other(e) => {
+                        error!("can't process {title_path}: {e:?}")
+                    }
+                },
+            }
+            drop(permit);
+        }));
+    }
+
+    for join_err in join_all(futs).await {
+        if let Err(e) = join_err {
+            error!("can't join task: {e:?}");
+        }
+    }
+
+    if let Err(e) = sqlx::query!(
+        r#"WITH _ AS (
+            DELETE FROM categories
+                WHERE id NOT IN (SELECT id FROM UNNEST($1::bigint[]))
+        )
+        DELETE FROM titles
+            WHERE id NOT IN (SELECT id FROM UNNEST($2::bigint[]))"#,
+        &category_path_to_id
+            .lock()
+            .await
+            .values()
+            .copied()
+            .collect::<Vec<i64>>(),
+        &*upserted_title_ids.lock().await,
+    )
+    .execute(&app_state.pool)
+    .await
+    {
+        error!("can't cleanup non-exist categories and titles from database: {e:?}");
+    };
+
+    info!("finished processing library");
+
+    Ok(())
 }
