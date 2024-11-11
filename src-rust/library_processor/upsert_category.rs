3c093375a035cac39d0b9b3b6dc486a6e2e54c32
Delnegend <kiennguyen19323@gmail.com>
2024-11-11 10:01:40 +0700

    refactor(`upsert_category`): rm unecessary raw string indicator and double quotes from query

--- src-rust/library_processor/upsert_category.rs
+++ src-rust/library_processor/upsert_category.rs
@@ -100,21 +100,19 @@ pub async fn upsert_category<'e>(
     }
 
     let category_id = sqlx::query!(
-        r#"
-        INSERT INTO "categories"
-            ("id", "name", "description", "path",
-            "cover_path", "cover_blurhash",
-            "cover_width", "cover_height")
+        "INSERT INTO categories
+            (id, name, description, path,
+            cover_path, cover_blurhash,
+            cover_width, cover_height)
         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-        ON CONFLICT ("path") DO UPDATE SET
-            "name" = EXCLUDED.name,
-            "description" = EXCLUDED.description,
-            "cover_path" = EXCLUDED.cover_path,
-            "cover_blurhash" = EXCLUDED.cover_blurhash,
-            "cover_width" = EXCLUDED.cover_width,
-            "cover_height" = EXCLUDED.cover_height
-        RETURNING id
-    "#,
+        ON CONFLICT (path) DO UPDATE SET
+            name = EXCLUDED.name,
+            description = EXCLUDED.description,
+            cover_path = EXCLUDED.cover_path,
+            cover_blurhash = EXCLUDED.cover_blurhash,
+            cover_width = EXCLUDED.cover_width,
+            cover_height = EXCLUDED.cover_height
+        RETURNING id",
         app_state
             .id_generator
             .snowflake()
