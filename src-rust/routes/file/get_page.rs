0ed66b99df91223288e302065570f7a9621ff5b8
Delnegend <kiennguyen19323@gmail.com>
2024-11-11 13:57:13 +0700

    refactor(`routes/file/get_page`): rm raw string markers; rename `result` to `record`; replace match w/ early return

--- src-rust/routes/file/get_page.rs
+++ src-rust/routes/file/get_page.rs
@@ -23,25 +23,27 @@ pub async fn get_page(
     State(app_state): State<Arc<AppState>>,
     Path(page_id): Path<i64>,
 ) -> Result<Response, AppError> {
-    let result = sqlx::query!(
-        r#"
-        SELECT
+    let record = match sqlx::query!(
+        "SELECT
             pages.path AS page_path,
             titles.path AS title_path,
             pages.filesize AS page_filesize
         FROM pages JOIN titles
             ON pages.title_id = titles.id
-        WHERE pages.id = $1
-        "#,
+        WHERE pages.id = $1",
         page_id
     )
-    .fetch_one(&app_state.pool)
+    .fetch_optional(&app_state.pool)
     .await
-    .context("title not found from page id")?;
+    .context("can't query page")?
+    {
+        Some(record) => record,
+        None => return Ok((StatusCode::NOT_FOUND).into_response()),
+    };
 
     let headers = [(
         header::CONTENT_TYPE,
-        match result.page_path.split('.').last().unwrap_or_default() {
+        match record.page_path.split('.').last().unwrap_or_default() {
             "" => "image".to_string(),
             "jpg" => "image/jpeg".to_string(),
             v => format!("image/{v}"),
@@ -49,7 +51,7 @@ pub async fn get_page(
     )];
 
     let content = Body::from_stream(
-        PathBuf::from(result.title_path).stream_file(result.page_path, result.page_filesize)?,
+        PathBuf::from(record.title_path).stream_file(record.page_path, record.page_filesize)?,
     );
 
     Ok((StatusCode::OK, headers, content).into_response())
