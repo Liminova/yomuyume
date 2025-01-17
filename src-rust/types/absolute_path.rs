7c7523da1b67951315e10984cf1866365da33226
Delnegend <kiennguyen19323@gmail.com>
2025-01-18 00:35:42 +0700

    refactor(`absolute_path`): replace anyhow w/ err enum; `to_absolute` trait for PathBuf

--- src-rust/types/absolute_path.rs
+++ src-rust/types/absolute_path.rs
@@ -1,15 +1,27 @@
 use std::{
     fmt::{Display, Formatter},
-    path::{Path, PathBuf},
+    path::{Path, PathBuf, StripPrefixError},
 };
-
-use anyhow::{Context, Result};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct AbsolutePath(PathBuf);
 
+#[derive(Debug, thiserror::Error)]
+pub enum AbsolutePathErr {
+    #[error("can't get current working dir: {0:?}")]
+    GetCurrentWorkingDir(std::io::Error),
+    #[error("can't canonicalize path: {0:?}")]
+    Canonicalize(std::io::Error),
+
+    #[error("can't strip prefix {0} from path {1}: {0:?}")]
+    StripPrefix(String, String, StripPrefixError),
+
+    #[error("can't strip cwd from path {0}: {1:?}")]
+    StripCwdPrefix(String, StripPrefixError),
+}
+
 impl AbsolutePath {
-    pub fn from(path: &Path, base: Option<&PathBuf>) -> Result<Self> {
+    pub fn from(path: &Path, base: Option<&PathBuf>) -> Result<Self, AbsolutePathErr> {
         if path.is_absolute() {
             return Ok(Self(path.to_path_buf()));
         }
@@ -17,15 +29,15 @@ impl AbsolutePath {
             Some(cwd) => Ok(Self(
                 cwd.join(path)
                     .canonicalize()
-                    .context("can't canonicalize path")?,
+                    .map_err(AbsolutePathErr::Canonicalize)?,
             )),
             None => {
-                let cwd = std::env::current_dir()?;
+                let cwd = std::env::current_dir().map_err(AbsolutePathErr::GetCurrentWorkingDir)?;
                 Ok(Self(
                     cwd.as_path()
                         .join(path)
                         .canonicalize()
-                        .context("can't canonicalize path")?,
+                        .map_err(AbsolutePathErr::Canonicalize)?,
                 ))
             }
         }
@@ -35,23 +47,27 @@ impl AbsolutePath {
         self.0.to_string_lossy().to_string()
     }
 
-    pub fn to_relative(&self, base: Option<&AbsolutePath>) -> Result<PathBuf> {
+    pub fn to_relative(&self, base: Option<&AbsolutePath>) -> Result<PathBuf, AbsolutePathErr> {
         match base {
             Some(base) => Ok(self
                 .0
                 .strip_prefix(base.as_ref())
-                .context(format!(
-                    "can't strip `{}` from `{}`",
-                    base.as_ref().display(),
-                    self.0.display()
-                ))?
+                .map_err(|e| {
+                    AbsolutePathErr::StripPrefix(
+                        base.to_string_lossy(),
+                        self.0.to_string_lossy().to_string(),
+                        e,
+                    )
+                })?
                 .to_path_buf()),
             None => {
-                let cwd = std::env::current_dir()?;
+                let cwd = std::env::current_dir().map_err(AbsolutePathErr::GetCurrentWorkingDir)?;
                 Ok(self
                     .0
                     .strip_prefix(cwd.as_path())
-                    .context(format!("can't strip cwd from `{}`", self.0.display()))?
+                    .map_err(|e| {
+                        AbsolutePathErr::StripCwdPrefix(self.0.to_string_lossy().to_string(), e)
+                    })?
                     .to_path_buf())
             }
         }
@@ -67,5 +83,15 @@ impl AsRef<PathBuf> for AbsolutePath {
 impl Display for AbsolutePath {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0.display())
+    }
+}
+
+pub trait ToAbsolute<T> {
+    fn to_absolute(&self, base: Option<&T>) -> Result<AbsolutePath, AbsolutePathErr>;
+}
+
+impl ToAbsolute<PathBuf> for PathBuf {
+    fn to_absolute(&self, base: Option<&PathBuf>) -> Result<AbsolutePath, AbsolutePathErr> {
+        AbsolutePath::from(self.as_path(), base)
     }
 }
