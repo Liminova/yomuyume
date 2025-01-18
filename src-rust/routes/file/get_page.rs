f06874aea2aa0726d49cf286b000a42d04ac4129
Delnegend <kiennguyen19323@gmail.com>
2025-01-19 00:14:18 +0700

    refactor(`get_page`): if bool else

--- src-rust/routes/file/get_page.rs
+++ src-rust/routes/file/get_page.rs
@@ -55,23 +55,20 @@ pub async fn get_page(
         },
     )];
 
-    let content = match record.title_is_dir {
-        true => {
-            let file_path = PathBuf::from(record.title_path).join(record.page_path);
-            let file = File::open(file_path)
-                .await
-                .context("can't open page file")?;
-            let stream = ReaderStream::new(file);
+    let content = if record.title_is_dir {
+        let file_path = PathBuf::from(record.title_path).join(record.page_path);
+        let file = File::open(file_path)
+            .await
+            .context("can't open page file")?;
+        let stream = ReaderStream::new(file);
 
-            Body::from_stream(stream)
-        }
-        false => {
-            let archive_file = PathBuf::from(record.title_path);
-            let stream =
-                archive_file.stream_file_from_archive(record.page_path, record.page_filesize)?;
+        Body::from_stream(stream)
+    } else {
+        let archive_file = PathBuf::from(record.title_path);
+        let stream =
+            archive_file.stream_file_from_archive(record.page_path, record.page_filesize)?;
 
-            Body::from_stream(stream)
-        }
+        Body::from_stream(stream)
     };
 
     Ok((StatusCode::OK, headers, content).into_response())
