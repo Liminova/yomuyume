2853fbf227bd084b12525d140bfe90ab87c97386
Delnegend <kiennguyen19323@gmail.com>
2025-01-22 12:37:01 +0700

    refactor(`components/utils.ts`): lint; add missing imports

--- src/components/home/utils.ts
+++ src/components/home/utils.ts
@@ -1,6 +1,13 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
 export const homeStore = defineStore("home", () => {
 	const coverHeight = ref(300);
-	const setCoverHeight = (newVal: number) => (coverHeight.value = newVal);
+
+	function setCoverHeight(newVal: number): void {
+		coverHeight.value = newVal;
+	}
+
 	const recommendsContainerHeight = 500;
 	const gapPixel = 18;
 	const snackbarMessage = ref("");
