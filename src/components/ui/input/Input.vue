b18c481b5f582d4004cbe4f5cb31b2dbb4772ebb
Delnegend <kiennguyen19323@gmail.com>
2025-01-22 11:34:04 +0700

    feat(`ui/Input`): md3 design

--- src/components/ui/input/Input.vue
+++ src/components/ui/input/Input.vue
@@ -1,26 +1,105 @@
 <script setup lang="ts">
 import { useVModel } from "@vueuse/core";
-import type { HTMLAttributes } from "vue";
+import { X } from "lucide-vue-next";
+import { computed, type HTMLAttributes, ref } from "vue";
 
-import { cn } from "~/lib/utils";
+import Toggle from "~/components/Toggle.vue";
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
 	defaultValue?: string | number;
 	modelValue?: string | number;
 	class?: HTMLAttributes["class"];
-}>();
+	supportingText?: string;
+	showSupportingText?: boolean;
+	label?: string;
+	type?: HTMLInputElement["type"];
+	disabled?: boolean;
+	style?: "default" | "destructive";
+}>(), {
+	defaultValue: "",
+	modelValue: undefined,
+	class: "",
+	type: "text",
+	supportingText: "",
+	showSupportingText: false,
+	label: "",
+	disabled: false,
+	style: "default",
+});
 
+/* eslint no-unused-vars: 0 */
 const emits = defineEmits<(e: "update:modelValue", payload: string | number)=> void>();
 
 const modelValue = useVModel(props, "modelValue", emits, {
 	passive: true,
 	defaultValue: props.defaultValue,
 });
+
+const isFocused = ref(false);
+const isLabelElevated = computed(() => {
+	return modelValue.value !== "" || isFocused.value;
+});
+
 </script>
 
 <template>
-	<input
-		v-model="modelValue"
-		:class="cn('flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', props.class)"
-	>
+	<div :class="props.class">
+		<div class="relative">
+			<input
+				ref="inputRef"
+				v-model="modelValue"
+				:placeholder="props.label"
+				:type="props.type"
+				:disabled="props.disabled"
+				class="peer flex h-10 w-full rounded-sm border bg-transparent px-3 py-2 duration-150 placeholder:text-transparent focus:outline-none focus-visible:border-2"
+				:class="{
+					'border-input focus-visible:border-primary': props.style === 'default',
+					'border-destructive focus-visible:border-destructive': props.style === 'destructive',
+				}"
+				:style="{
+					'transition-property': 'border-color, border-width',
+					'transition-timing-function': 'cubic-bezier(0.4, 0, 0.2, 1)'
+				}"
+				@focus="isFocused = true"
+				@blur="isFocused = false">
+
+			<!-- text-muted-foreground peer-focus:text-primary -->
+			<div
+				class="pointer-events-none absolute bg-background text-sm duration-150"
+				:class="{
+					'left-3 top-0 -translate-y-2 px-1 text-xs': isLabelElevated,
+					'left-4 top-1/2 -translate-y-1/2': !isLabelElevated,
+					'text-destructive/80': props.style === 'destructive' && !isLabelElevated && !props.disabled,
+					'text-destructive': props.style === 'destructive' && isLabelElevated && !props.disabled,
+					'text-muted-foreground': props.style === 'default' && !isLabelElevated && !props.disabled,
+					'text-primary': (props.style === 'default' && isLabelElevated) && !props.disabled,
+					'text-input': props.disabled,
+				}"
+				:style="{
+					'transition-property': 'color, left, top, transform, font-size, padding',
+					'transition-timing-function': 'cubic-bezier(0.4, 0, 0.2, 1)'
+				}">
+				{{ props.label }}
+			</div>
+
+			<!-- right icon -->
+			<div
+				v-if="modelValue !== ''"
+				class="absolute right-2 top-1/2 -translate-y-1/2 rounded-full border-2 border-primary p-1">
+				<X :size="10" />
+			</div>
+		</div>
+
+		<!-- supporting text -->
+		<Toggle
+			:show="props.showSupportingText"
+			dimension="vertical"
+			class="h-6 w-full px-4 py-1 text-xs"
+			:class="{
+				'text-muted-foreground': props.style === 'default',
+				'text-destructive': props.style === 'destructive',
+			}">
+			{{ props.supportingText }}
+		</Toggle>
+	</div>
 </template>
