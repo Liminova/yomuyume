7906c2b7bb08fb643be494e913b99825708790db
Delnegend <kiennguyen19323@gmail.com>
2025-01-24 12:18:50 +0700

    chore(`eslint`): simpler config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,22 @@
 import hagemanto from "eslint-plugin-hagemanto";
 import pluginVue from 'eslint-plugin-vue';
-import globals from "globals";
 
 export default [
 	{ name: "yomuyume/files", files: ["src/**/*.{ts,vue}"] },
 	{ name: "yomuyume/ignores", ignores: ["**/*.d.ts"] },
 
 	...hagemanto({
-		"styler": "stylistic",
-		"enableJsx": false,
-		"enableTs": true,
-		"enableTailwind": true,
-		"sortImports": true
+		styler: "stylistic",
+		enableJsx: false,
+		extraFileExtensions: [".vue"],
 	}),
+
 	...pluginVue.configs['flat/recommended'],
 
 	{
 		name: "yomuyume/specific",
 		rules: {
 			"tailwindcss/no-custom-classname": "off",
-			"indent": ["error", "tab"],
 			"no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
 			"vue/html-indent": ["error", "tab"],
 			"vue/multi-word-component-names": "off",
@@ -34,15 +31,6 @@ export default [
 					}
 				}
 			],
-		}
-	},
-
-	{
-		name: "yomuyume/language-options",
-		languageOptions: {
-			globals: globals.browser, parserOptions: {
-				project: true, parser: "@typescript-eslint/parser", extraFileExtensions: [".vue"]
-			}
 		}
 	},
 ]
