d4b3c0479d25227350265f763aa37017b04e4518
Delnegend <kiennguyen19323@gmail.com>
2025-01-25 15:08:34 +0700

    refactor(`post_search`): `None` if 0 for numbers

--- src-rust/routes/content/post_search.rs
+++ src-rust/routes/content/post_search.rs
@@ -179,12 +179,20 @@ pub async fn post_search(
         }),
         date_updated: r.date_updated.map(|d| d.to_rfc3339()),
         tags: r.tags.tags_split(),
-        favorites: r.favorites_count,
-        bookmarks: r.bookmarks_count,
+        favorites: if r.favorites_count != 0 {
+            Some(r.favorites_count)
+        } else {
+            None
+        },
+        bookmarks: if r.bookmarks_count != 0 {
+            Some(r.favorites_count)
+        } else {
+            None
+        },
 
         is_favorite: r.is_favorite,
         is_bookmark: r.is_bookmark,
-        page_read: r.page_read,
+        page_read: r.page_read.filter(|i| *i != 0),
     })
     .collect::<Vec<_>>();
 
