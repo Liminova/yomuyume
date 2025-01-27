ec5f3f11a9faef2efeb0b990fa31c610759b365a
Delnegend <kiennguyen19323@gmail.com>
2025-01-27 16:24:24 +0700

    feat(`app_error`): replace anyhow w/ enum & thiserror

--- src-rust/utils/app_error.rs
+++ src-rust/utils/app_error.rs
@@ -1,32 +1,36 @@
-use std::fmt::Display;
+use std::fmt::Debug;
 
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
 };
 
-#[derive(Debug)]
-pub struct AppError(anyhow::Error);
+use crate::utils::{archive_file::ArchiveFileError, id_generator::GenerateIDErr};
+
+#[derive(Debug, thiserror::Error)]
+pub enum AppError {
+    #[error("database error: {0}")]
+    DB(sqlx::Error),
+    #[error("io error: {0}")]
+    IO(std::io::Error),
+    #[error("archive error: {0}")]
+    Archive(ArchiveFileError),
+    #[error("mailer error: {0}")]
+    Mailer(anyhow::Error),
+
+    #[error("cache error: can't get {0} to read, this should not happen")]
+    ReadCache(String),
+    #[error("cache error: can't get {0} to write, this should not happen")]
+    WriteCache(String),
+
+    #[error("can't generate snowflake id: {0}")]
+    Snowflake(GenerateIDErr),
+    #[error("can't hash password: {0}")]
+    PasswordHash(argon2::password_hash::errors::Error),
+}
+
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
-        tracing::error!("{:?}", self.0);
-
-        (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            format!("something went wrong: {:?}", self.0),
-        )
-            .into_response()
-    }
-}
-
-impl Display for AppError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}", self.0)
-    }
-}
-
-impl From<anyhow::Error> for AppError {
-    fn from(err: anyhow::Error) -> Self {
-        Self(err)
+        (StatusCode::INTERNAL_SERVER_ERROR, format!("{self:?}")).into_response()
     }
 }
