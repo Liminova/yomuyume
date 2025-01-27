37f95ef6e21db7701701a0772535080df649fc2e
Delnegend <kiennguyen19323@gmail.com>
2025-01-27 16:29:24 +0700

    refactor(`components/ImagePoly`): rm old image process pipeline

--- src/components/ImagePoly/workers/blurhash.web.ts
+++
@@ -1,5 +0,0 @@
-import blurhashDecode from "../decodePipeline/blurhash";
-
-self.onmessage = async (event: MessageEvent<[string, number, number]>): Promise<void> => {
-	self.postMessage(await blurhashDecode(event.data));
-};
--- src/components/ImagePoly/workers/image.web.ts
+++
@@ -1,7 +0,0 @@
-import imageDecode from "../decodePipeline/image";
-
-type MyMessageData = [string, string, string]; /** src, format, jwt token */
-
-self.onmessage = async (event: MessageEvent<MyMessageData>): Promise<void> => {
-	self.postMessage(await imageDecode(event.data));
-};
