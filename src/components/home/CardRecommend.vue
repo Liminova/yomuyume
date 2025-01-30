d9f298529d3a864e70774e01ed51d2cae0103d57
Delnegend <kiennguyen19323@gmail.com>
2025-01-30 18:36:04 +0700

    refactor(`CardRecommend.vue`): simpler props; restyle to work w/ new carousel

--- src/components/home/CardRecommend.vue
+++ src/components/home/CardRecommend.vue
@@ -1,46 +1,22 @@
 <script setup lang="ts">
-
 import { NuxtLink } from "#components";
 import Image from "~/components/Image.vue";
 import type { GetTitleResponseBody } from "~/composables/api/content";
 import { toCoverApiEndpoint } from "~/composables/api/file";
-import type { MyImage } from "~/lib/types";
 
-import { homeStore } from "./utils";
-
-const props = withDefaults(defineProps<{
-	title: GetTitleResponseBody;
-	isFirstTitle?: boolean;
-	isLastTitle?: boolean;
-}>(), {
-	isFirstTitle: true,
-	isLastTitle: false,
-});
-
-const store = homeStore();
+const props = defineProps<{ title: GetTitleResponseBody }>();
 </script>
 
 <template>
 	<NuxtLink
 		:to="`/title/${props.title.id}`"
-		class="relative flex h-full flex-row justify-center overflow-hidden bg-black/50 sm:static"
-		:class="{
-			'rounded-l-3xl': props.isFirstTitle,
-			'rounded-r-3xl': props.isLastTitle,
-		}">
+		class="relative flex flex-row justify-center overflow-hidden rounded-3xl bg-black/50 max-sm:aspect-[10/12] sm:static">
 		<!-- Background -->
-		<div
-			class="absolute left-0 top-0 z-[-1] hidden w-full overflow-hidden sm:block"
-			:class="{
-				'rounded-l-3xl': props.isFirstTitle,
-				'rounded-r-3xl': props.isLastTitle,
-			}"
-			:style="{ height: `${store.recommendsContainerHeight}px` }">
+		<div class="absolute z-[-1] hidden size-full overflow-hidden object-cover px-2 sm:block">
 			<Image
 				:id="props.title.id"
-				class="w-full scale-110 overflow-hidden object-cover blur-sm"
+				class="size-full rounded-3xl object-cover"
 				:draggable="false"
-				image-class="overflow-hidden"
 				:src="toCoverApiEndpoint(props.title.id)"
 				:blurhash="props.title.cover_blurhash"
 				:width="props.title.cover_width"
@@ -49,12 +25,11 @@ const store = homeStore();
 		</div>
 
 		<!-- Cover -->
-		<div class="size-full sm:min-w-[350px] sm:max-w-xs lg:py-10 lg:pl-10">
+		<div class="size-full sm:max-lg:max-w-56 lg:max-w-sm lg:py-10 lg:pl-10">
 			<Image
 				:id="props.title.id"
+				class="size-full overflow-hidden object-cover sm:aspect-[10/16] lg:rounded-2xl"
 				:draggable="false"
-				class="h-full overflow-hidden lg:rounded-2xl"
-				image-class="h-full object-cover"
 				:src="toCoverApiEndpoint(props.title.id)"
 				:blurhash="props.title.cover_blurhash"
 				:width="props.title.cover_width"
@@ -63,7 +38,7 @@ const store = homeStore();
 		</div>
 
 		<div
-			class="pointer-events-none absolute left-0 top-0 flex size-full bg-black/50 sm:hidden" />
+			class="pointer-events-none absolute left-0 top-0 flex size-full bg-gradient-to-t from-black/80 to-transparent sm:hidden" />
 
 		<!-- Informations -->
 		<div
