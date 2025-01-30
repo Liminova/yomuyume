175805495451a290d94de8107ca078a1c1116017
Delnegend <kiennguyen19323@gmail.com>
2025-01-31 04:12:05 +0700

    refactor(`assets`): rm unused libs

--- src/assets/polyfill/avif_dec.d.ts
+++
@@ -1,7 +0,0 @@
-export type AVIFModule = EmscriptenWasm.Module & {
-	decode(data: BufferSource): ImageData | null;
-};
-
-declare let moduleFactory: EmscriptenWasm.ModuleFactory<AVIFModule>;
-
-export default moduleFactory;
--- src/assets/polyfill/jxl_dec.d.ts
+++
@@ -1,7 +0,0 @@
-export type JXLModule = EmscriptenWasm.Module & {
-	decode(data: BufferSource): ImageData | null;
-};
-
-declare let moduleFactory: EmscriptenWasm.ModuleFactory<JXLModule>;
-
-export default moduleFactory;
