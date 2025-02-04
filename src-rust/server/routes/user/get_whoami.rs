5ba59e10dec142addb430a76dfa378929b3e450e
Delnegend <kiennguyen19323@gmail.com>
2025-02-04 17:41:04 +0700

    refactor(`get_whoami`): use; utoipa; rename structs

--- src-rust/server/routes/user/get_whoami.rs
+++ src-rust/server/routes/user/get_whoami.rs
@@ -10,14 +10,11 @@ use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 use utoipa::ToSchema;
 
-use crate::{
-    types::UserID,
-    utils::{app_error::AppError, app_state::AppState},
-};
+use crate::{routes::errors::InternalError, types::id::UserID, utils::app_state::AppState};
 
 #[skip_serializing_none]
 #[derive(Debug, Clone, ToSchema, Serialize, Deserialize)]
-pub struct WhoAmIResponseBody {
+pub struct WhoAmIResponse {
     pub user_id: i64,
     pub username: String,
     pub email: String,
@@ -27,27 +24,25 @@ pub struct WhoAmIResponseBody {
     pub verified_at: Option<String>,
 }
 
-/// whoami
-///
-/// get the current user's information
+/// Get current logged in user
 #[utoipa::path(get, path = "/api/user/whoami", responses(
-    (status = 200, description = "get whoami successful", body = WhoAmIResponseBody),
-    (status = 401, description = "unauthorized", body = String),
-    (status = 500, description = "internal server error", body = String),
+    (status = 200, description = "Get whoami successful", body = WhoAmIResponse),
+    (status = 401, description = "Unauthorized", body = String),
+    (status = 500, description = "Internal server error", body = String),
 ), security(("session-id" = [], "session-secret" = [])))]
 pub async fn get_whoami(
     State(app_state): State<Arc<AppState>>,
     Extension(user_id): Extension<UserID>,
-) -> Result<Response, AppError> {
+) -> Result<Response, InternalError> {
     let user = app_state.user_cache.get(&user_id).ok_or_else(|| {
-        let e = AppError::ReadCache("user".to_string());
+        let e = InternalError::ReadCache("user".to_string());
         tracing::error!("{e:?}");
         e
     })?;
 
     Ok((
         StatusCode::OK,
-        Json(WhoAmIResponseBody {
+        Json(WhoAmIResponse {
             user_id: user_id.as_ref(),
             username: user.username.clone(),
             email: user.email.clone(),
