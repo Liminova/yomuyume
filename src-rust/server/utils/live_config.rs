9cd528c34379dbb30c234bc36618ffdee804a665
Delnegend <kiennguyen19323@gmail.com>
2025-02-04 17:47:13 +0700

    refactor(`live_config`): atomic lock; config IDs enum; simpler getter/setter fn

--- src-rust/server/utils/live_config.rs
+++ src-rust/server/utils/live_config.rs
@@ -1,50 +1,101 @@
 #![allow(clippy::struct_excessive_bools)]
 
+use std::fmt::Display;
+
 use chrono::Utc;
+use tokio::sync::RwLock;
+
+use crate::routes::errors::InternalError;
 
 /// Like [`Config`], but configurable from the frontend.
 #[derive(Debug)]
 pub struct LiveConfig {
-    pub nomedia_support: bool,
+    nomedia_support: RwLock<bool>,
 
-    pub komga_oneshot_support: bool,
-    pub komga_recycle_support: bool,
+    komga_oneshot_support: RwLock<bool>,
+    komga_recycle_support: RwLock<bool>,
 
-    pub rescan_enabled: bool,
-    pub rescan_interval_in_minutes: i32,
+    rescan_enabled: RwLock<bool>,
+    rescan_interval_in_minutes: RwLock<u32>,
+}
+
+#[derive(Debug)]
+pub enum LiveConfigItem {
+    NomediaSupport,
+    KomgaOneshotSupport,
+    KomgaRecycleSupport,
+    RescanEnabled,
+    RescanIntervalInMinutes,
+}
+
+use LiveConfigItem as Item;
+
+impl Display for Item {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Item::NomediaSupport => write!(f, "nomedia_support"),
+            Item::KomgaOneshotSupport => write!(f, "komga_oneshot_support"),
+            Item::KomgaRecycleSupport => write!(f, "komga_recycle_support"),
+            Item::RescanEnabled => write!(f, "rescan_enabled"),
+            Item::RescanIntervalInMinutes => write!(f, "rescan_interval_in_minutes"),
+        }
+    }
+}
+
+impl PartialEq<String> for Item {
+    fn eq(&self, other: &String) -> bool {
+        &self.to_string() == other
+    }
+}
+
+impl PartialEq<Item> for String {
+    fn eq(&self, other: &Item) -> bool {
+        self == &other.to_string()
+    }
 }
 
 impl LiveConfig {
-    pub async fn from_db(db: &sqlx::PgPool) -> Result<Self, sqlx::Error> {
-        let nomedia_support =
-            sqlx::query!("SELECT value FROM live_config WHERE id = 'nomedia_support'")
-                .fetch_optional(db)
-                .await?
-                .map_or_else(|| false, |record| record.value == "true");
+    pub async fn load(db: &sqlx::PgPool) -> Result<Self, sqlx::Error> {
+        let records = sqlx::query!(
+            r#"SELECT id,
+                value
+            FROM live_config
+            WHERE id IN (
+                    SELECT id
+                    FROM UNNEST($1::TEXT [])
+                )"#,
+            &[
+                Item::NomediaSupport.to_string(),
+                Item::KomgaOneshotSupport.to_string(),
+                Item::KomgaRecycleSupport.to_string(),
+                Item::RescanEnabled.to_string(),
+                Item::RescanIntervalInMinutes.to_string(),
+            ]
+        )
+        .fetch_all(db)
+        .await?;
 
-        let komga_oneshot_support =
-            sqlx::query!("SELECT value FROM live_config WHERE id = 'komga_oneshot_support'")
-                .fetch_optional(db)
-                .await?
-                .map_or_else(|| false, |record| record.value == "true");
+        macro_rules! get_bool {
+            ($id:expr, $default:expr) => {
+                RwLock::new(
+                    records
+                        .iter()
+                        .find(|r| r.id == $id)
+                        .map_or($default, |r| r.value == "true"),
+                )
+            };
+        }
 
-        let komga_recycle_support =
-            sqlx::query!("SELECT value FROM live_config WHERE id = 'komga_recycle_support'")
-                .fetch_optional(db)
-                .await?
-                .map_or_else(|| false, |record| record.value == "true");
-
-        let rescan_enabled =
-            sqlx::query!("SELECT value FROM live_config WHERE id = 'rescan_enabled'")
-                .fetch_optional(db)
-                .await?
-                .map_or_else(|| true, |record| record.value == "true");
-
-        let rescan_interval_in_minutes =
-            sqlx::query!("SELECT value FROM live_config WHERE id = 'rescan_interval_in_minutes'")
-                .fetch_optional(db)
-                .await?
-                .map_or_else(|| 60, |record| record.value.parse::<i32>().unwrap_or(60));
+        let nomedia_support = get_bool!(Item::NomediaSupport, false);
+        let komga_oneshot_support = get_bool!(Item::KomgaOneshotSupport, false);
+        let komga_recycle_support = get_bool!(Item::KomgaRecycleSupport, false);
+        let rescan_enabled = get_bool!(Item::RescanEnabled, true);
+        let rescan_interval_in_minutes = RwLock::new(
+            records
+                .iter()
+                .find(|r| r.id == Item::RescanIntervalInMinutes)
+                .map_or(60, |r| r.value.parse::<u32>().unwrap_or(60)),
+        );
 
         Ok(Self {
             nomedia_support,
@@ -55,118 +106,79 @@ impl LiveConfig {
         })
     }
 
-    pub async fn configure_nomedia_support(
-        &mut self,
+    pub async fn set(
+        &self,
         db: &sqlx::PgPool,
-        value: bool,
-    ) -> Result<(), sqlx::Error> {
+        config: &Item,
+        value: &str,
+    ) -> Result<(), InternalError> {
+        match (&config, value) {
+            (Item::RescanIntervalInMinutes, value) => {
+                if value.parse::<u32>().is_err() {
+                    return Err(InternalError::LiveConfig(
+                        "value must be an integer".to_string(),
+                    ));
+                };
+            }
+            (_, value) if value != "true" && value != "false" => {
+                return Err(InternalError::LiveConfig(
+                    "value must be 'true' or 'false'".to_string(),
+                ));
+            }
+            _ => {}
+        };
+
         sqlx::query!(
             "INSERT INTO live_config (id, value, last_updated_at)
             VALUES ($1, $2, $3) ON CONFLICT (id) DO
             UPDATE
             SET value = $2,
                 last_updated_at = $3",
-            "nomedia_support",
-            if value { "true" } else { "false" },
+            config.to_string(),
+            value,
             Utc::now()
         )
         .execute(db)
-        .await?;
+        .await
+        .map_err(|e| {
+            tracing::error!("{e:?}");
+            InternalError::DB(e)
+        })?;
 
-        self.nomedia_support = value;
+        match config {
+            Item::NomediaSupport => *self.nomedia_support.write().await = value == "true",
+            Item::KomgaOneshotSupport => {
+                *self.komga_oneshot_support.write().await = value == "true";
+            }
+            Item::KomgaRecycleSupport => {
+                *self.komga_recycle_support.write().await = value == "true";
+            }
+            Item::RescanEnabled => *self.rescan_enabled.write().await = value == "true",
+            Item::RescanIntervalInMinutes => {
+                *self.rescan_interval_in_minutes.write().await = value.parse().unwrap_or(60);
+            }
+        }
 
         Ok(())
     }
 
-    pub async fn configure_komga_oneshot_support(
-        &mut self,
-        db: &sqlx::PgPool,
-        value: bool,
-    ) -> Result<(), sqlx::Error> {
-        sqlx::query!(
-            "INSERT INTO live_config (id, value, last_updated_at)
-            VALUES ($1, $2, $3) ON CONFLICT (id) DO
-            UPDATE
-            SET value = $2,
-                last_updated_at = $3",
-            "komga_oneshot_support",
-            if value { "true" } else { "false" },
-            Utc::now()
-        )
-        .execute(db)
-        .await?;
-
-        self.komga_oneshot_support = value;
-
-        Ok(())
+    pub async fn get_nomediasupport(&self) -> bool {
+        *self.nomedia_support.read().await
     }
 
-    pub async fn configure_komga_recycle_support(
-        &mut self,
-        db: &sqlx::PgPool,
-        value: bool,
-    ) -> Result<(), sqlx::Error> {
-        sqlx::query!(
-            "INSERT INTO live_config (id, value, last_updated_at)
-            VALUES ($1, $2, $3) ON CONFLICT (id) DO
-            UPDATE
-            SET value = $2,
-                last_updated_at = $3",
-            "komga_recycle_support",
-            if value { "true" } else { "false" },
-            Utc::now()
-        )
-        .execute(db)
-        .await?;
-
-        self.komga_recycle_support = value;
-
-        Ok(())
+    pub async fn get_komga_oneshot_support(&self) -> bool {
+        *self.komga_oneshot_support.read().await
     }
 
-    pub async fn configure_rescan_enabled(
-        &mut self,
-        db: &sqlx::PgPool,
-        value: bool,
-    ) -> Result<(), sqlx::Error> {
-        sqlx::query!(
-            "INSERT INTO live_config (id, value, last_updated_at)
-            VALUES ($1, $2, $3) ON CONFLICT (id) DO
-            UPDATE
-            SET value = $2,
-                last_updated_at = $3",
-            "rescan_enabled",
-            if value { "true" } else { "false" },
-            Utc::now()
-        )
-        .execute(db)
-        .await?;
-
-        self.rescan_enabled = value;
-
-        Ok(())
+    pub async fn get_komga_recycle_support(&self) -> bool {
+        *self.komga_recycle_support.read().await
     }
 
-    pub async fn configure_rescan_interval_in_minutes(
-        &mut self,
-        db: &sqlx::PgPool,
-        value: i32,
-    ) -> Result<(), sqlx::Error> {
-        sqlx::query!(
-            "INSERT INTO live_config (id, value, last_updated_at)
-            VALUES ($1, $2, $3) ON CONFLICT (id) DO
-            UPDATE
-            SET value = $2,
-                last_updated_at = $3",
-            "rescan_interval_in_minutes",
-            format!("{}", value),
-            Utc::now()
-        )
-        .execute(db)
-        .await?;
+    pub async fn get_rescan_enabled(&self) -> bool {
+        *self.rescan_enabled.read().await
+    }
 
-        self.rescan_interval_in_minutes = value;
-
-        Ok(())
+    pub async fn get_rescan_interval_in_minutes(&self) -> u32 {
+        *self.rescan_interval_in_minutes.read().await
     }
 }
