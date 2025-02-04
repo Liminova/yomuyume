b37c6def10bc2205ebd790c711b1de3099cbd7ea
Delnegend <kiennguyen19323@gmail.com>
2025-02-04 17:26:24 +0700

    refactor(`routes/content/mod`): mod; use

--- src-rust/server/routes/content/mod.rs
+++ src-rust/server/routes/content/mod.rs
@@ -1,7 +1,15 @@
 mod get_categories;
-mod get_title;
+mod get_chapter;
+mod get_oneshot;
+mod get_series;
+mod get_tags;
 mod post_search;
+mod types;
 
 pub use get_categories::*;
-pub use get_title::*;
+pub use get_chapter::*;
+pub use get_oneshot::*;
+pub use get_series::*;
+pub use get_tags::*;
 pub use post_search::*;
+pub use types::*;
