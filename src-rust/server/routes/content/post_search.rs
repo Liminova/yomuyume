7f45c33d36c3127893897c57ee90e40463aef80a
Delnegend <kiennguyen19323@gmail.com>
2025-02-04 17:24:00 +0700

    refactor(`post_search`): use; utoipa; rename structs; clippy

--- src-rust/server/routes/content/post_search.rs
+++ src-rust/server/routes/content/post_search.rs
@@ -1,11 +1,6 @@
 use std::sync::Arc;
 
-use crate::{
-    routes::TitleResponseBody,
-    traits::tags_split::TagsSplit,
-    types::UserID,
-    utils::{app_error::AppError, app_state::AppState},
-};
+use crate::{routes::errors::InternalError, types::id::UserID, utils::app_state::AppState};
 
 use axum::{
     extract::State,
@@ -16,8 +11,10 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use utoipa::ToSchema;
 
+use super::types::BaseTitleResponse;
+
 #[derive(Debug, ToSchema, Serialize, Deserialize, Default)]
-pub enum OrderBy {
+pub enum InnerSearchRequestOrderBy {
     #[serde(rename = "title")]
     #[default]
     Title,
@@ -27,18 +24,18 @@ pub enum OrderBy {
     UpdateDate,
 }
 
-impl AsRef<str> for OrderBy {
+impl AsRef<str> for InnerSearchRequestOrderBy {
     fn as_ref(&self) -> &str {
         match self {
-            OrderBy::Title => "title",
-            OrderBy::ReleaseDate => "release_date",
-            OrderBy::UpdateDate => "update_date",
+            InnerSearchRequestOrderBy::Title => "title",
+            InnerSearchRequestOrderBy::ReleaseDate => "release_date",
+            InnerSearchRequestOrderBy::UpdateDate => "update_date",
         }
     }
 }
 
 #[derive(Debug, ToSchema, Serialize, Deserialize)]
-pub struct SearchRequestBody {
+pub struct SearchRequest {
     pub term: Option<String>,
 
     pub category_ids: Option<Vec<String>>,
@@ -47,32 +44,38 @@ pub struct SearchRequestBody {
 
     pub offset: Option<i64>,
     pub limit: Option<i64>,
-    pub order_by: Option<OrderBy>,
+    pub order_by: Option<InnerSearchRequestOrderBy>,
     pub is_ascending: Option<bool>,
 }
 
 #[derive(Debug, ToSchema, Serialize, Deserialize)]
-pub struct SearchResponseBody {
-    pub data: Option<Vec<TitleResponseBody>>,
+pub struct InnerTitleSearchResponse {
+    pub is_series: bool,
+
+    #[serde(flatten)]
+    pub base: BaseTitleResponse,
+}
+
+#[derive(Debug, ToSchema, Serialize, Deserialize)]
+pub struct SearchResponse {
+    pub data: Option<Vec<InnerTitleSearchResponse>>,
 
     pub offset: i64,
     pub limit: i64,
 }
 
-/// quick search for titles by keywords
-///
-/// and also sorting them by various options
+/// Search title
 #[utoipa::path(post, path = "api/content/search", responses(
-    (status = 200, description = "fetch all items successful", body = SearchResponseBody),
-    (status = 204, description = "fetch all items successful, but none were found", body = SearchResponseBody),
-    (status = 401, description = "unauthorized", body = String),
-    (status = 500, description = "internal server error", body = String)
+    (status = 200, description = "Search success", body = SearchResponse),
+    (status = 204, description = "Search success, but none were found", body = SearchResponse),
+    (status = 401, description = "Unauthorized", body = String),
+    (status = 500, description = "Internal server error", body = String)
 ), security(("session-id" = [], "session-secret" = [])))]
 pub async fn post_search(
     State(app_state): State<Arc<AppState>>,
     Extension(user_id): Extension<UserID>,
-    Json(request_body): Json<SearchRequestBody>,
-) -> Result<Response, AppError> {
+    Json(request_body): Json<SearchRequest>,
+) -> Result<Response, InternalError> {
     let limit = request_body.limit.unwrap_or(10);
     let offset = request_body.offset.unwrap_or(0);
     let order_by = request_body.order_by.unwrap_or_default();
@@ -112,7 +115,9 @@ pub async fn post_search(
             t.cover_height AS cover_height,
             t.date_updated AS date_updated,
             t.path AS path,
-            ARRAY_AGG(DISTINCT CONCAT(tg.id, '-', tg.name)) FILTER (
+            ARRAY_AGG(
+                json_build_array(tg.id, tg.name)
+            ) FILTER (
                 WHERE tg.name IS NOT NULL
             ) AS "tags",
             (
@@ -203,48 +208,60 @@ pub async fn post_search(
     .await
     .map_err(|e| {
         tracing::error!("{e:?}");
-        AppError::DB(e)
+        InternalError::DB(e)
     })?
     .into_iter()
-    .map(|r| TitleResponseBody {
-        id: r.id.to_string(),
-        title: r.title,
-        author: r.author,
-        category_id: r.category_id.map(|i| i.to_string()),
-        description: r.description,
-        release: r.release.map(|d| d.format("%Y-%m-%d").to_string()),
+    .map(|r| InnerTitleSearchResponse {
         is_series: r.is_series,
+        base: BaseTitleResponse {
+            id: r.id.to_string(),
+            title: r.title,
+            author: r.author,
+            category_id: r.category_id.map(|i| i.to_string()),
+            description: r.description,
+            release: r.release.map(|d| d.format("%Y-%m-%d").to_string()),
 
-        cover_blurhash: r.cover_blurhash,
-        cover_width: r.cover_width,
-        cover_height: r.cover_height,
-        cover_jxl: r.cover_path.map(|p| {
-            std::path::Path::new(&p)
-                .extension()
-                .is_some_and(|ext| ext.eq_ignore_ascii_case("jxl"))
-        }),
-        date_updated: r.date_updated.map(|d| d.to_rfc3339()),
-        tags: r.tags.tags_split(),
-        favorites: if r.favorites_count != 0 {
-            Some(r.favorites_count)
-        } else {
-            None
-        },
-        bookmarks: if r.bookmarks_count != 0 {
-            Some(r.favorites_count)
-        } else {
-            None
-        },
+            cover_blurhash: r.cover_blurhash,
+            cover_width: r.cover_width,
+            cover_height: r.cover_height,
+            cover_jxl: r.cover_path.map(|p| {
+                std::path::Path::new(&p)
+                    .extension()
+                    .is_some_and(|ext| ext.eq_ignore_ascii_case("jxl"))
+            }),
+            date_updated: r.date_updated.map(|d| d.to_rfc3339()),
+            tags: r.tags.map(|mut tags| {
+                tags.iter_mut()
+                    .filter_map(|tag| {
+                        let mut pair = tag.as_array()?.iter();
+                        Some((
+                            pair.next()?.as_i64()?.to_string(),
+                            pair.next()?.as_str()?.to_string(),
+                        ))
+                    })
+                    .collect()
+            }),
+            favorites: if r.favorites_count != 0 {
+                Some(r.favorites_count)
+            } else {
+                None
+            },
+            bookmarks: if r.bookmarks_count != 0 {
+                Some(r.favorites_count)
+            } else {
+                None
+            },
 
-        is_favorite: r.is_favorite,
-        is_bookmark: r.is_bookmark,
-        page_read: r.page_read.filter(|i| *i != 0),
+            is_favorite: r.is_favorite,
+            is_bookmark: r.is_bookmark,
+            page_read: r.page_read.filter(|i| *i != 0),
+        },
     })
     .collect::<Vec<_>>();
 
     Ok((
         StatusCode::OK,
-        Json(SearchResponseBody {
+        Json(SearchResponse {
             data: if data.is_empty() { None } else { Some(data) },
             offset,
             limit,
