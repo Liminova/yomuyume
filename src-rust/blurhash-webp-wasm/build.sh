2a74bccc40cc224ed8722c583858c4bfd698bb5a
Delnegend <kiennguyen19323@gmail.com>
2025-03-31 19:18:29 +0700

    chore: rm unused build scripts

--- src-rust/blurhash-webp-wasm/build.sh
+++
@@ -1,4 +0,0 @@
-cd /workspaces/yomuyume/src-server/blurhash-webp-wasm
-wasm-pack build --target web --out-dir /workspaces/yomuyume/src/components/image/blurhash-webp-wasm
-cd -
-rm /workspaces/yomuyume/src/components/image/blurhash-webp-wasm/.gitignore
--- src-rust/jxl-webp-wasm/build.sh
+++
@@ -1,4 +0,0 @@
-cd /workspaces/yomuyume/src-server/jxl-webp-wasm
-wasm-pack build --target web --out-dir /workspaces/yomuyume/src/components/image/jxl-webp-wasm
-cd -
-rm /workspaces/yomuyume/src/components/image/jxl-webp-wasm/.gitignore
