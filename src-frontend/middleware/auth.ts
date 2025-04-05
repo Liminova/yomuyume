2252b5b798d6de17c22fe7d3e5d58fe49d1fc8fb
Delnegend <kiennguyen19323@gmail.com>
2025-04-06 04:00:25 +0700

    refactor(`fe/middleware/auth`): use lib

--- src-frontend/middleware/auth.ts
+++ src-frontend/middleware/auth.ts
@@ -1,24 +1,10 @@
 import { defineNuxtRouteMiddleware, navigateTo } from "nuxt/app";
 
-import { NewYomuyumeRequest, WHOAMI_PATH } from "~/composables/api/common";
+import { isLoggedIn } from "~/lib/is-logged-in";
 
 export default defineNuxtRouteMiddleware(async (to, _) => {
-	const loggedIn = await new Promise<boolean>(resolve => NewYomuyumeRequest(WHOAMI_PATH)
-		.then(() => {
-			resolve(true);
-		})
-		.catch((e: unknown) => {
-			if (typeof e === "object"
-				&& e !== null
-				&& "message" in e
-				&& typeof e.message === "string"
-				&& e.message.includes("[401]")) {
-				resolve(false);
-			}
-			throw e;
-		}),
-	);
-	const atAuthRoute = to.path === "/auth";
+	const loggedIn = await isLoggedIn();
+	const atAuthRoute = to.path === "/auth/login";
 	if (!loggedIn && !atAuthRoute) {
 		return navigateTo("/auth");
 	}
