249f5998188cd8b90523778d421eeed4276d7934
Delnegend <kiennguyen19323@gmail.com>
2025-04-06 04:49:58 +0700

    fix(`fe/tailwind.config`): content field

--- src-frontend/tailwind.config.js
+++ src-frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/*.vue"],
+	content: ["app.vue", "components/**/*.vue", "layouts/**/*.vue", "pages/**/*.vue"],
 	darkMode: ["class"],
 	safelist: ["dark"],
 	prefix: "",
