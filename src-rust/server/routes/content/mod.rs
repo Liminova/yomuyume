f2b14ce62a4452f6aa65908909aad208e20362fa
Delnegend <kiennguyen19323@gmail.com>
2025-04-06 04:58:12 +0700

    refactor(`be/routes/content/mod`): organize use; rm `parse_pages`; type alias

--- src-rust/server/routes/content/mod.rs
+++ src-rust/server/routes/content/mod.rs
@@ -1,21 +1,21 @@
 mod get_categories;
-mod get_chapter;
-mod get_oneshot;
-mod get_series;
+mod get_pages;
+mod get_search;
 mod get_tags;
-mod post_search;
+mod get_title;
 mod structs;
 
 pub use get_categories::*;
-pub use get_chapter::*;
-pub use get_oneshot::*;
-pub use get_series::*;
+pub use get_pages::*;
+pub use get_search::*;
 pub use get_tags::*;
-pub use post_search::*;
+pub use get_title::*;
 use serde_json::Value;
 pub use structs::*;
 
-fn parse_tags(vals: Vec<Value>) -> Option<Vec<(String, String)>> {
+type TagID = String;
+type TagName = String;
+fn parse_tags(vals: Vec<Value>) -> Option<Vec<(TagID, TagName)>> {
     let mut tags = vals
         .into_iter()
         .filter_map(|tag| {
@@ -38,34 +38,4 @@ fn is_jxl(p: impl AsRef<str>) -> bool {
     std::path::Path::new(p.as_ref())
         .extension()
         .is_some_and(|ext| ext.eq_ignore_ascii_case("jxl"))
-}
-
-fn parse_pages(vals: Vec<Value>) -> Vec<BasePageResponse> {
-    let mut pages: Vec<(String, BasePageResponse)> = vals
-        .into_iter()
-        .filter_map(|r| {
-            let page = r.as_object()?;
-            let path = page.get("path")?.as_str()?;
-
-            Some((
-                path.to_string(),
-                BasePageResponse {
-                    id: page.get("id")?.as_i64()?.to_string(),
-                    blurhash: None,
-                    width: None,
-                    height: None,
-                    jxl: is_jxl(path),
-                    description: page
-                        .get("description")
-                        .and_then(|s| s.as_str())
-                        .map(|s| s.to_string()),
-                },
-            ))
-        })
-        .collect();
-
-    pages.sort_by(|a, b| a.0.cmp(&b.0));
-    pages.dedup_by(|a, b| a.0 == b.0);
-
-    pages.into_iter().map(|(_, page)| page).collect()
 }
