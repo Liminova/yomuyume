dfb27c7e9aa9803cd9fef5f52ae0f95eb5cd3801
Delnegend <kiennguyen19323@gmail.com>
2025-04-08 01:48:25 +0700

    feat(`fe/pages/bookmarks,continue,favorites`): basic layout w/ real data

--- src-frontend/pages/bookmarks.vue
+++ src-frontend/pages/bookmarks.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
+import { computed } from "vue";
+
 import { definePageMeta } from "#imports";
+import TitleCard from "~/components/title/TitleCard.vue";
+import { BookmarkedTitlesQuery, useContentSearchTitle } from "~/composables/api/content";
 
 definePageMeta({ layout: "nav-drawer", middleware: ["auth"] });
+
+const titlesInf = useContentSearchTitle(BookmarkedTitlesQuery);
+const titles = computed(() => titlesInf.data.value?.pages.flatMap(page => page.data ?? []) ?? []);
 </script>
 
 <template>
-	<div>Bookmarks</div>
+	<div class="grid grid-cols-[repeat(auto-fill,minmax(12rem,1fr))] gap-4">
+		<div
+			v-for="title in titles"
+			:key="title.id">
+			<TitleCard :title="title" />
+		</div>
+	</div>
 </template>
