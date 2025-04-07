71d11b28171da2e6dbcc6d8971911e4c7156b5bc
Delnegend <kiennguyen19323@gmail.com>
2025-04-08 01:43:20 +0700

    feat(`fe/components/TitlePagesArea`): reading progress

--- src-frontend/components/title/TitlePagesArea.vue
+++ src-frontend/components/title/TitlePagesArea.vue
@@ -1,12 +1,55 @@
 <script setup lang="ts">
 
+import { watchEffect } from "vue";
+import { toast } from "vue-sonner";
+
 import Image from "~/components/image/Image.vue";
 import { GET_PAGE_FILE_PATH } from "~/composables/api/common";
-import { useContentGetPages } from "~/composables/api/content";
+import { type BaseTitleResponse, type TitleResponse, useContentGetPages } from "~/composables/api/content";
+import { useUserSetProgress } from "~/composables/api/user";
 
-const props = defineProps<{ chapterId: string }>();
+const props = defineProps<{
+	titleId: string;
+	chapterId: string;
+	chapterIds: Array<{ id: string }>;
+}>();
 
 const pages = useContentGetPages(props.chapterId);
+const setProgress = useUserSetProgress();
+function handleImageEmitInview(pageId: string): void {
+	if (!pages.data.value) { return; }
+
+	const chapterIdx = props.chapterIds.findIndex(chapter => chapter.id === props.chapterId);
+	if (chapterIdx === -1) { return; }
+	const chapterCount = props.chapterIds.length;
+	let chapterPercent = 0;
+	if (chapterCount > 1) {
+		chapterPercent = chapterIdx / chapterCount;
+	}
+
+	const pageIdx = pages.data.value.findIndex(page => page.id === pageId);
+	const pagePercent = (pageIdx + 1) / pages.data.value.length;
+
+	setProgress.mutate({
+		title_id: props.titleId,
+		chapter_id: props.chapterId,
+		page_id: pageId,
+		percent: Math.round((chapterPercent + (pagePercent / chapterCount)) * 100),
+	}, {
+		onError: (error) => {
+			toast.error("Failed to update progress", {
+				description: error.message,
+			});
+		},
+	});
+}
+
+watchEffect(() => {
+	if (!pages.error.value) { return; }
+	toast.error("Can't load pages", {
+		description: pages.error.value,
+	});
+});
 </script>
 
 <template>
@@ -26,7 +69,9 @@ const pages = useContentGetPages(props.chapterId);
 				:src="GET_PAGE_FILE_PATH(page.id)"
 				:is-jxl="page.jxl"
 				:width="page.width"
-				:height="page.height" />
+				:height="page.height"
+				:emit-when-in-view="true"
+				@in-view="() => { handleImageEmitInview(page.id); }" />
 		</div>
 	</div>
 </template>
