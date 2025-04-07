cf5d0c3c514f45b3a80a894779609d9cce678f63
Delnegend <kiennguyen19323@gmail.com>
2025-04-08 01:48:42 +0700

    feat(`fe/app`): tanstack query devtools

--- src-frontend/app.vue
+++ src-frontend/app.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
+import { VueQueryDevtools } from "@tanstack/vue-query-devtools";
+
 import { NuxtPwaManifest } from "#components";
 import Sonner from "~/components/ui/Sonner.vue";
 import { useTheme } from "~/composables/use-theme";
-
 useTheme();
 </script>
 
 <template>
 	<NuxtLayout>
 		<NuxtPwaManifest />
+		<VueQueryDevtools />
 		<Sonner />
 		<NuxtPage />
 	</NuxtLayout>
