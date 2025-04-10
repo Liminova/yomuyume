b48ba3862db52b086b1109ab1c323ddd701c0e91
Delnegend <kiennguyen19323@gmail.com>
2025-04-10 17:00:17 +0700

    refactor(`fe/app`): rename fns; await init jxl native

--- src-frontend/app.vue
+++ src-frontend/app.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { NuxtPwaManifest } from "#components";
 import Sonner from "~/components/ui/Sonner.vue";
-import { useThemeWatcher } from "~/composables/use-theme";
+import { initTheme } from "~/composables/use-theme";
 
-import { useDrawerStoreWatcher } from "./composables/use-drawer-store";
+import { initIsJxlNative } from "./components/image/is-jxl-native";
+import { initDrawerStore } from "./composables/use-drawer-store";
 
-useDrawerStoreWatcher();
-useThemeWatcher();
+initDrawerStore();
+initTheme();
+await initIsJxlNative();
 </script>
 
 <template>
