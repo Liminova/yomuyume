dc4d3b5e2c5ed856b96f5b61f9aeb4215f9a8f97
Delnegend <kiennguyen19323@gmail.com>
2025-04-10 16:59:29 +0700

    perf(`fe/components/image/is-jxl-native`): combine w/ `useState` to avoid re-validation every call

--- src-frontend/components/image/is-jxl-native.ts
+++ src-frontend/components/image/is-jxl-native.ts
@@ -1,7 +1,12 @@
 import { useAsyncData, useState } from "nuxt/app";
 
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type
 export function useIsJxlNative() {
-	return useAsyncData("is-jxl-native", async (): Promise<boolean> => {
+	return useState("is-jxl-native", () => true);
+}
+/** Run this function once in `app.vue` */
+export async function initIsJxlNative(): Promise<void> {
+	const { data } = await useAsyncData<boolean>("is-jxl-native", async (): Promise<boolean> => {
 		const image = new Image();
 
 		image.src = `data:image/jxl;base64,/woIAAAMABKIAgC4AF3lEgAAFSqjjBu8nOv58kOHxbSN6wxttW1hSaLIODZJJ3BIEkkaoCUzGM6qJAE=`;
@@ -15,5 +20,11 @@ export function useIsJxlNative() {
 				resolve(false);
 			};
 		});
+	}, {
+		server: false,
+		immediate: true,
 	});
+
+	const isJxlNative = useIsJxlNative();
+	isJxlNative.value = data.value ?? true;
 }
