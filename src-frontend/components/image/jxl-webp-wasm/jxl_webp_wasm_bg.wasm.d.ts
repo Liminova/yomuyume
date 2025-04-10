ba8118e4609e98d1d4a877121c25e1738f690f9f
Delnegend <kiennguyen19323@gmail.com>
2025-04-10 16:22:38 +0700

    refactor: gitignore generated files

--- src-frontend/components/image/jxl-webp-wasm/jxl_webp_wasm_bg.wasm.d.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export const decode: (a: number, b: number) => [number, number];
-export const __wbindgen_free: (a: number, b: number, c: number) => void;
-export const __wbindgen_malloc: (a: number, b: number) => number;
-export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
-export const __wbindgen_export_3: WebAssembly.Table;
-export const __wbindgen_start: () => void;
