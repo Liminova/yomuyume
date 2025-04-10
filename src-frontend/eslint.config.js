82fe63b492cb1d7db93c3d3da9e1451393871c92
Delnegend <kiennguyen19323@gmail.com>
2025-04-10 17:00:55 +0700

    build(`fe/eslint`): reformat; disable fn return type on api composables

--- src-frontend/eslint.config.js
+++ src-frontend/eslint.config.js
@@ -2,25 +2,30 @@ import hagemanto from "eslint-plugin-hagemanto";
 import pluginVue from 'eslint-plugin-vue';
 
 export default [
-	{ name: "yomuyume/files", files: ["**/*.{ts,vue}"] },
-	{ name: "yomuyume/ignores", ignores: ["**/*.d.ts", "**/*.js", "node_modules", ".nuxt", ".output", "assets", "public", "server", "*.config.*"] },
-
-	...hagemanto({
+	{
+		name: "yomuyume/files",
+		files: ["**/*.{ts,vue}"]
+	}, {
+		name: "yomuyume/ignores",
+		ignores: ["**/*.d.ts", "**/*.js", "node_modules", ".nuxt", ".output", "assets", "public", "server", "*.config.*"]
+	}, ...hagemanto({
 		enableJsx: false,
 		vueConfig: pluginVue.configs['flat/recommended']
-	}),
-
-	{
+	}), {
 		name: "yomuyume/specific",
 		rules: {
-			"tailwindcss/no-custom-classname": 0,
 			"no-unused-vars": 0,
+			"tailwindcss/no-custom-classname": 0,
 			"class-methods-use-this": 0,
 			"@typescript-eslint/class-methods-use-this": 0,
 		}
-	},
-
-	{
+	}, {
+		files: ["composables/api/**/*.ts"],
+		rules: {
+			"@typescript-eslint/explicit-module-boundary-types": 0,
+			"@typescript-eslint/explicit-function-return-type": 0,
+		}
+	}, {
 		languageOptions: {
 			globals: {
 				window: "writable",
