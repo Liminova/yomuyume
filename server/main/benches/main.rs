2c5c326cadb7d5f6449e1efa4f38ab9213e9256e
Delnegend <kiennguyen19323@gmail.com>
2025-09-01 00:02:00 +0700

    chore(`server/main/benches`): rm batch upsert since we no longer use postgres

--- server/main/benches/main.rs
+++ server/main/benches/main.rs
@@ -1,6 +1,5 @@
-mod batch_upsert;
 mod vec_rev_or_deque;
 use criterion::{criterion_group, criterion_main};
 
-criterion_group!(benches, batch_upsert::main, vec_rev_or_deque::main);
+criterion_group!(benches, vec_rev_or_deque::main);
 criterion_main!(benches);
