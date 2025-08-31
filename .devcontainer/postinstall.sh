d87b820acb3a716bd7c653687d54db4905d76033
Delnegend <kiennguyen19323@gmail.com>
2025-08-31 18:53:05 +0700

    build(`.devcontainer/postinstall.sh`): upgrade 7z

--- .devcontainer/postinstall.sh
+++ .devcontainer/postinstall.sh
@@ -98,10 +98,11 @@ if [ ! -d /usr/local/lib/dav1d-$ver/build ]; then
 fi
 cd /usr/local/lib/dav1d-$ver/build && sudo ninja install
 
-ver="2408"
+# 7zip
+ver="2501"
 curl -L -o /tmp/7z.tar.xz https://www.7-zip.org/a/7z$ver-linux-x64.tar.xz
 checksum=$(openssl dgst -sha3-512 /tmp/7z.tar.xz | awk '{print $2}')
-expected="5c0e23cc2575219d5a2962cdf66adfc117454291f80e6786c55267ff1c076e645fb62dbe1d7ab60de9a9de473af602d1e4dbf3cdaaf210379fb84ba1e9eb4996"
+expected="20ab025c487de16840ca4b4a5783ae62bda66ddbb4488ea4a91c0e521cee9dc90513a93bc52694c1c23e7208c6f03608e1a418c6612ebed19437114fa3933cee"
 if [ ! "$checksum" = "$expected" ]; then
     echo "7z tarball checksum failed\nexpected: $expected\ngot: $checksum"
 else
