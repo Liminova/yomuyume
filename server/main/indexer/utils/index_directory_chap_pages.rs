81821c2e8061cdf650adb0cdeb425a8a56fa9482
Delnegend <kiennguyen19323@gmail.com>
2025-09-02 02:41:36 +0700

    refactor(`server/indexer/index_dir_chap_pages`): rename to `files_in_chapter`, mark it optional

--- server/main/indexer/utils/index_directory_chap_pages.rs
+++ server/main/indexer/utils/index_directory_chap_pages.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, fs::DirEntry, sync::Arc};
 use chrono::{DateTime, Utc};
 use image::{GenericImageView, ImageReader};
 use rayon::prelude::*;
-use tracing::warn;
+use tracing::{error, warn};
 
 use crate::{
     AppState,
@@ -29,79 +29,99 @@ struct PageInDir {
 pub async fn read_chap_pages_dir(
     app_state: &Arc<AppState>,
     chapter_path: &AbsolutePath,
-    files_in_dir: Vec<DirEntry>,
+    files_in_chapter: Option<Vec<DirEntry>>,
     pages_in_db: &Vec<(PageIdentityPath, database::content::PageInfo)>,
 ) -> Result<IndexedChapterPages, UpsertTitleErr> {
-    let pages_in_dir = files_in_dir.into_iter().fold(Vec::new(), |mut acc, e| {
-        let path = e.path();
-        let Some(abs_path) = AbsolutePath::from(&e.path(), None).okay(|err| {
-            warn!("can't convert {} to absolute path: {err}", path.display());
-        }) else {
-            return acc;
-        };
-        let Some(rel_path) = abs_path
-            .to_relative(Some(chapter_path))
-            .map(|p| p.to_string_lossy().to_string())
-            .okay(|e| {
-                warn!("can't convert {} to relative path: {e}", path.display());
-            })
-        else {
-            return acc;
-        };
-
-        if abs_path.is_file() {
-            acc.push(PageInDir {
-                path: abs_path,
-                rel_path,
-                last_modified: path.last_modified().okay(|err| {
-                    warn!("can't get last modified for {}: {err}", path.display());
-                }),
-                size: e
-                    .metadata()
-                    .okay(|err| {
-                        warn!("can't get metadata for {}: {err}", path.display());
-                    })
-                    .map(|m| m.len() as i64),
-            });
-            return acc;
-        }
-        acc.append(
-            path.scan_dir_recursively_for_image(app_state.config.feature_nomedia)
-                .into_iter()
-                .fold(&mut Vec::new(), |acc, img| {
-                    let Some(abs_path) = AbsolutePath::from(&img, None).okay(|err| {
-                        warn!("can't convert {} to absolute path: {err}", img.display());
-                    }) else {
-                        return acc;
-                    };
-                    let Some(rel_path) = abs_path
-                        .to_relative(Some(chapter_path))
-                        .map(|p| p.to_string_lossy().to_string())
-                        .okay(|e| {
-                            warn!(
-                                "can't convert {} to relative path: {e}",
-                                abs_path.as_ref().display()
-                            );
+    let pages_in_dir = files_in_chapter
+        .unwrap_or_else(|| {
+            std::fs::read_dir(chapter_path.as_ref())
+                .map(|rd| {
+                    rd.filter_map(|f| {
+                        f.okay(|e| {
+                            error!("can't read dir entry in {}: {e}", chapter_path.display())
                         })
-                    else {
-                        return acc;
-                    };
+                    })
+                    .collect()
+                })
+                .okay(|e| {
+                    error!(
+                        "can't read chapter directory {}: {e}",
+                        chapter_path.display()
+                    )
+                })
+                .unwrap_or_default()
+        })
+        .into_iter()
+        .fold(Vec::new(), |mut acc, e| {
+            let path = e.path();
+            let Some(abs_path) = AbsolutePath::from(&e.path(), None).okay(|err| {
+                warn!("can't convert {} to absolute path: {err}", path.display());
+            }) else {
+                return acc;
+            };
+            let Some(rel_path) = abs_path
+                .to_relative(Some(chapter_path))
+                .map(|p| p.to_string_lossy().to_string())
+                .okay(|e| {
+                    warn!("can't convert {} to relative path: {e}", path.display());
+                })
+            else {
+                return acc;
+            };
 
-                    acc.push(PageInDir {
-                        path: abs_path,
-                        rel_path,
-                        last_modified: img.last_modified().okay(|err| {
-                            warn!("can't get last modified for {}: {err}", img.display());
-                        }),
-                        size: img.metadata().map(|m| m.len() as i64).okay(|err| {
-                            warn!("can't get metadata for {}: {err}", img.display());
-                        }),
-                    });
-                    acc
-                }),
-        );
-        acc
-    });
+            if abs_path.is_file() {
+                acc.push(PageInDir {
+                    path: abs_path,
+                    rel_path,
+                    last_modified: path.last_modified().okay(|err| {
+                        warn!("can't get last modified for {}: {err}", path.display());
+                    }),
+                    size: e
+                        .metadata()
+                        .okay(|err| {
+                            warn!("can't get metadata for {}: {err}", path.display());
+                        })
+                        .map(|m| m.len() as i64),
+                });
+                return acc;
+            }
+            acc.append(
+                path.scan_dir_recursively_for_image(app_state.config.feature_nomedia)
+                    .into_iter()
+                    .fold(&mut Vec::new(), |acc, img| {
+                        let Some(abs_path) = AbsolutePath::from(&img, None).okay(|err| {
+                            warn!("can't convert {} to absolute path: {err}", img.display());
+                        }) else {
+                            return acc;
+                        };
+                        let Some(rel_path) = abs_path
+                            .to_relative(Some(chapter_path))
+                            .map(|p| p.to_string_lossy().to_string())
+                            .okay(|e| {
+                                warn!(
+                                    "can't convert {} to relative path: {e}",
+                                    abs_path.as_ref().display()
+                                );
+                            })
+                        else {
+                            return acc;
+                        };
+
+                        acc.push(PageInDir {
+                            path: abs_path,
+                            rel_path,
+                            last_modified: img.last_modified().okay(|err| {
+                                warn!("can't get last modified for {}: {err}", img.display());
+                            }),
+                            size: img.metadata().map(|m| m.len() as i64).okay(|err| {
+                                warn!("can't get metadata for {}: {err}", img.display());
+                            }),
+                        });
+                        acc
+                    }),
+            );
+            acc
+        });
     if pages_in_dir.is_empty() {
         return Err(UpsertTitleErr::IsEmpty);
     }
