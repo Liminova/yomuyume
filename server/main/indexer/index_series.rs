af22a3ded398ba4d0eff5f37e7a8431755e1ab04
Delnegend <kiennguyen19323@gmail.com>
2025-09-04 17:17:48 +0700

    refactor(`server/indexer/index_series`): less nesting; use utils trait

--- server/main/indexer/index_series.rs
+++ server/main/indexer/index_series.rs
@@ -24,8 +24,8 @@ use crate::{
         },
     },
     utils::{
-        absolute_path::AbsolutePath, archive_file::ArchiveFile, average_color::HexColor,
-        comic_info::ComicInfo, constants::COMICINFO, result_utils::ResultUtils,
+        absolute_path::AbsolutePath, average_color::HexColor, pathbuf_utils::PathBufUtils,
+        result_utils::ResultUtils,
     },
 };
 
@@ -110,114 +110,75 @@ pub async fn index_series(
                 .map(|v| v.value());
 
             let pages_in_db = app_state
-                .indexer.first_time
-                .then(|| Vec::new())
+                .indexer
+                .first_time
+                .then(|| vec![])
                 .unwrap_or_else(|| {
+                    let Some(chapter_info) = chapter_info else {
+                        return vec![];
+                    };
                     chapter_info
-                        .map(|c| c.pages)
-                        .map(|page_identity_paths| {
-                            page_identity_paths
-                                .iter()
-                                .filter_map(|page_identity_path| {
-                                    pages_table
-                                        .get(page_key(
-                                            category_identity_path.clone(),
-                                            title_identity_path.clone(),
-                                            Some(chapter_identity_path.clone()),
-                                            page_identity_path.clone(),
-                                        ))
-                                        .okay(|e| warn!("can't get PageInfo: {e:?}"))
-                                        .flatten()
-                                        .map(|p| (page_identity_path.clone(), p.value()))
-                                })
-                                .collect::<Vec<_>>()
+                        .pages
+                        .iter()
+                        .filter_map(|page_identity_path| {
+                            pages_table
+                                .get(page_key(
+                                    category_identity_path.clone(),
+                                    title_identity_path.clone(),
+                                    Some(chapter_identity_path.clone()),
+                                    page_identity_path.clone(),
+                                ))
+                                .okay(|e| warn!("can't get PageInfo: {e:?}"))
+                                .flatten()
+                                .map(|p| (page_identity_path.clone(), p.value()))
                         })
-                        .unwrap_or_default()
+                        .collect::<Vec<_>>()
                 });
 
-            match partial_indexed_chapter.kind {
-                IndexedChapterKind::Directory => {
-                    let chapter_comic_info_path = chapter_path.as_ref().join(COMICINFO);
-                    read_chap_pages_dir(&app_state, &chapter_path, None, &pages_in_db)
-                        .await
-                        .map(|pages| IndexedChapter {
-                            identity_path: chapter_identity_path,
-                            fallback_vol_num: partial_indexed_chapter.fallback_vol_num,
-                            last_modified: chapter_path.last_modified().okay(|e| {
-                                warn!("can't get last modified of {chapter_path_display}: {e}")
-                            }),
-                            cover: find_chapter_cover(
-                                &pages,
-                                chapter_comic_info_path
-                                    .exists()
-                                    .then(|| {
-                                        std::fs::read_to_string(chapter_comic_info_path).okay(|e| {
-                                            warn!(
-                                                "can't read ComicInfo.xml from chapter {chapter_path_display}: {e}"
-                                            )
-                                        })
-                                    })
-                                    .flatten()
-                                    .and_then(|s| {
-                                        ComicInfo::from_str(&s).okay(|e| {
-                                            warn!(
-                                                "can't parse ComicInfo.xml from chapter {chapter_path_display}: {e}"
-                                            )
-                                        })
-                                    })
-                                    .as_ref()
-                                    .map(|ci| ci.pages().as_slice()),
-                                &pages_in_db,
-                            ),
-                            path: chapter_path,
-                            pages,
-                        })
-                }
+            if let IndexedChapterKind::Archive(items_in_archive) = partial_indexed_chapter.kind {
+                let comic_info = chapter_path
+                    .as_ref()
+                    .read_comic_info_from_archive()
+                    .okay(|e| warn!("can't read ComicInfo.xml from chapter: {e}"));
 
-                IndexedChapterKind::Archive(items_in_archive) => read_chap_pages_archive(
-                    &app_state,
-                    &chapter_path,
-                    items_in_archive,
-                    &pages_in_db,
-                )
-                .await
-                .map(|pages| IndexedChapter {
-                    identity_path: chapter_identity_path,
-                    fallback_vol_num: partial_indexed_chapter.fallback_vol_num,
-                    last_modified: chapter_path.last_modified().okay(|e| {
-                        warn!("can't get last modified of {}: {e}", chapter_path_display)
-                    }),
-                    cover: find_chapter_cover(
-                        &pages,
-                        chapter_path
-                            .as_ref()
-                            .read_file_from_archive(COMICINFO)
-                            .okay(|e| {
-                                warn!(
-                                    "can't read ComicInfo.xml from chapter {chapter_path_display}: {e}"
-                                )
-                            })
-                            .and_then(|b| {
-                                String::from_utf8(b).okay(|e| {
-                                    warn!(
-                                        "can't convert ComicInfo.xml from chapter {chapter_path_display} to UTF-8: {e}"
-                                    )
-                                })
-                            })
-                            .and_then(|s| {
-                                ComicInfo::from_str(&s).okay(|e| {
-                                    warn!(
-                                        "can't parse ComicInfo.xml from chapter {chapter_path_display}: {e}"
-                                    )
-                                })
-                            })
-                            .as_ref()
-                            .map(|ci| ci.pages().as_slice()),
-                        &pages_in_db,
-                    ),
-                    path: chapter_path,
-                    pages,
-                })
+                read_chap_pages_archive(&app_state, &chapter_path, items_in_archive, &pages_in_db)
+                    .await
+                    .map(|pages| IndexedChapter {
+                        identity_path: chapter_identity_path,
+                        fallback_vol_num: partial_indexed_chapter.fallback_vol_num,
+                        last_modified: chapter_path.last_modified().okay(|e| {
+                            warn!("can't get last modified of {}: {e}", chapter_path_display)
+                        }),
+                        cover: find_chapter_cover(
+                            &pages,
+                            comic_info.as_ref().map(|ci| ci.pages().as_slice()),
+                            &pages_in_db,
+                        ),
+                        path: chapter_path,
+                        pages,
+                    })
+            } else {
+                let comic_info = chapter_path
+                    .as_ref()
+                    .read_comic_info_from_dir()
+                    .okay(|e| warn!("can't read ComicInfo.xml from chapter: {e}"));
+
+                read_chap_pages_dir(&app_state, &chapter_path, None, &pages_in_db)
+                    .await
+                    .map(|pages| IndexedChapter {
+                        identity_path: chapter_identity_path,
+                        fallback_vol_num: partial_indexed_chapter.fallback_vol_num,
+                        last_modified: chapter_path.last_modified().okay(|e| {
+                            warn!("can't get last modified of {chapter_path_display}: {e}")
+                        }),
+                        cover: find_chapter_cover(
+                            &pages,
+                            comic_info.as_ref().map(|ci| ci.pages().as_slice()),
+                            &pages_in_db,
+                        ),
+                        path: chapter_path,
+                        pages,
+                    })
             }
         });
     }
@@ -303,18 +264,7 @@ pub async fn index_series(
                         .map(|c| c.identity_path.clone())
                         .collect::<Vec<_>>(),
                 ),
-                // TODO: handle directory
-                last_modified: title_path
-                    .is_file()
-                    .then(|| {
-                        title_path.last_modified().okay(|e| {
-                            warn!(
-                                "can't get last modified of title {}: {e}",
-                                title_path.display()
-                            )
-                        })
-                    })
-                    .flatten(),
+                last_modified: None,
             },
         );
     }
@@ -339,7 +289,6 @@ pub async fn index_series(
                         .collect(),
                     fallback_vol_num: Some(indexed_chapter.fallback_vol_num),
                     cover: indexed_chapter.cover.clone(),
-                    last_modified: indexed_chapter.last_modified,
                 },
             );
         }
@@ -397,21 +346,12 @@ pub async fn index_series(
         .commit()
         .okay(|e| error!("can't commit write transaction: {e:?}"))?;
 
-    let comic_info = std::fs::read_to_string(title_path.as_ref().join(COMICINFO))
-        .okay(|e| {
-            warn!(
-                "can't read ComicInfo.xml from title {}: {e}",
-                title_path.display()
-            )
-        })
-        .and_then(|s| {
-            ComicInfo::from_str(&s).okay(|e| {
-                warn!(
-                    "can't parse ComicInfo.xml from title {}: {e}",
-                    title_path.display()
-                )
-            })
-        });
+    let comic_info = title_path.as_ref().read_comic_info_from_dir().okay(|e| {
+        warn!(
+            "can't read ComicInfo.xml from title {}: {e}",
+            title_path.display()
+        )
+    });
 
     comic_info_to_tantivy(
         &app_state,
