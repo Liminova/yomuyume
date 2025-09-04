8c76ccbae9b2f549b4c4b535dbd8585f988acfb0
Delnegend <kiennguyen19323@gmail.com>
2025-09-04 09:06:54 +0700

    refactor(`server/routes/user`): tidy up `Mailer`

--- server/main/routes/user/mod.rs
+++ server/main/routes/user/mod.rs
@@ -10,69 +10,66 @@ pub use post_modify::*;
 pub use post_sensitive::*;
 pub use put_progress::*;
 
-use anyhow::{Result, anyhow};
-use email_address::EmailAddress;
 use lettre::{
-    Message, SmtpTransport, Transport,
+    Address, Message, SmtpTransport, Transport,
+    address::AddressError,
     message::{IntoBody, Mailbox, header::ContentType},
-    transport::smtp::{authentication::Credentials, response::Response as LetterResponse},
+    transport::smtp::{self, authentication::Credentials, response::Response as LetterResponse},
 };
 
-use crate::utils::config::Config;
+use crate::config::Smtp;
 
 pub struct Mailer {
     sender: Mailbox,
     mailer: SmtpTransport,
 }
 
+#[derive(Debug, thiserror::Error)]
+pub enum MailerError {
+    #[error("invalid email: {0}")]
+    InvalidEmail(#[from] AddressError),
+
+    #[error("email transport error: {0}")]
+    TransportError(#[from] smtp::Error),
+
+    #[error("other error: {0}")]
+    Other(#[from] lettre::error::Error),
+}
+
 impl Mailer {
-    pub fn from(env: &Config) -> Result<Self> {
-        let smtp = env
-            .smtp
-            .as_ref()
-            .ok_or_else(|| anyhow!("SMTP was not fully configured, contact the server owner"))?;
-
-        if !EmailAddress::is_valid(&smtp.from_email) {
-            return Err(anyhow!("Invalid SMTP from email, contact the server owner"));
-        }
-
-        let sender: Mailbox = format!("{} <{}>", smtp.from_name, smtp.from_email)
-            .parse()
-            .map_err(|_| anyhow!("invalid sender info, contact the server owner"))?;
-
-        let mailer = SmtpTransport::relay(&smtp.host)
-            .map_err(|e| anyhow!("can't create mailer: {}", e))?
-            .credentials(Credentials::new(
-                smtp.username.to_string(),
-                smtp.password.to_string(),
-            ))
-            .build();
-
-        #[allow(clippy::inconsistent_struct_constructor)]
-        Ok(Self { mailer, sender })
+    pub fn from(smtp: &Smtp) -> Result<Mailer, MailerError> {
+        Ok(Mailer {
+            mailer: SmtpTransport::relay(&smtp.host)
+                .map_err(MailerError::TransportError)?
+                .credentials(Credentials::new(
+                    smtp.username.clone(),
+                    smtp.password.clone(),
+                ))
+                .build(),
+            sender: Mailbox {
+                name: Some(smtp.from_name.clone()),
+                email: smtp.from_email.clone(),
+            },
+        })
     }
 
     pub fn send(
         &self,
-        receiver_name: impl AsRef<str>,
-        receiver_email: impl AsRef<str>,
+        receiver_name: Option<String>,
+        receiver_email: Address,
         subject: impl AsRef<str>,
         body: impl IntoBody,
-    ) -> Result<LetterResponse> {
-        let to: Mailbox = format!("{} <{}>", receiver_name.as_ref(), receiver_email.as_ref())
-            .parse()
-            .map_err(|_| anyhow!("invalid receiver info: {}", receiver_email.as_ref()))?;
-
-        let email = Message::builder()
-            .from(self.sender.clone())
-            .to(to)
-            .header(ContentType::TEXT_PLAIN)
-            .subject(subject.as_ref())
-            .body(body)
-            .map_err(|e| anyhow!("can't build email: {e}"))?;
-
-        self.mailer
-            .send(&email)
-            .map_err(|e| anyhow!("can't send email: {e}"))
+    ) -> Result<LetterResponse, MailerError> {
+        Ok(self.mailer.send(
+            &Message::builder()
+                .from(self.sender.clone())
+                .to(Mailbox {
+                    name: receiver_name,
+                    email: receiver_email,
+                })
+                .header(ContentType::TEXT_PLAIN)
+                .subject(subject.as_ref())
+                .body(body)?,
+        )?)
     }
 }
