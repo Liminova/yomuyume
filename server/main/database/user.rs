a36a8fce6573641acf46f642dd8d0583aa46b0c5
Delnegend <kiennguyen19323@gmail.com>
2025-09-04 17:18:41 +0700

    feat(`server/database/user`): more tables; use helper macros

--- server/main/database/user.rs
+++ server/main/database/user.rs
@@ -1,50 +1,71 @@
 use chrono::{DateTime, Utc};
-use redb::TableDefinition as TableDef;
+use redb::{MultimapTableDefinition as MultimapTableDef, TableDefinition as TableDef};
+use redb_macros::{RedbJsonValue, key_function};
 use serde::{Deserialize, Serialize};
+use serde_with::skip_serializing_none;
 
-#[derive(Debug, Serialize, Deserialize)]
+use crate::database::content::TitleKey;
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, RedbJsonValue)]
 pub struct UserInfo {
-    email: Option<String>,
-    password_hash: Option<String>,
-    profile_picture: Option<Vec<u8>>,
-    verified_at: Option<DateTime<Utc>>,
+    pub email: String,
+    pub name: Option<String>,
+    pub password_hash: String,
+    pub profile_picture: Option<Vec<u8>>,
+    pub verified_at: Option<DateTime<Utc>>,
 }
 
-impl redb::Value for UserInfo {
-    type SelfType<'a> = UserInfo;
-    type AsBytes<'a> = Vec<u8>;
+pub type UserID = String;
+pub type UserEmail = String;
 
-    fn fixed_width() -> Option<usize> {
-        None
-    }
+pub const USERS: TableDef<UserID, UserInfo> = TableDef::new("user_info");
 
-    fn from_bytes<'a>(data: &'a [u8]) -> Self::SelfType<'a>
-    where
-        Self: 'a,
-    {
-        if let Ok(data) = serde_json::from_slice::<UserInfo>(data) {
-            data
-        } else {
-            unreachable!("Failed to deserialize UserInfo, database corruption?")
+pub const USER_EMAIL_TO_ID: TableDef<UserEmail, UserID> = TableDef::new("user_email_to_id");
+
+#[skip_serializing_none]
+#[derive(Debug, Serialize, Deserialize, RedbJsonValue)]
+pub struct Session {
+    pub device: Option<String>,
+    pub session_secret: String,
+}
+
+impl redb::Key for Session {
+    fn compare(data1: &[u8], data2: &[u8]) -> std::cmp::Ordering {
+        use serde_json::from_slice;
+
+        if let Ok(data1) = from_slice::<Session>(data1)
+            && let Ok(data2) = from_slice::<Session>(data2)
+        {
+            return data1.session_secret.cmp(&data2.session_secret);
         }
-    }
-
-    fn as_bytes<'a, 'b: 'a>(value: &'a Self::SelfType<'b>) -> Self::AsBytes<'a>
-    where
-        Self: 'b,
-    {
-        if let Ok(data) = serde_json::to_vec(value) {
-            data
-        } else {
-            unreachable!("Failed to serialize UserInfo, memory corruption?")
-        }
-    }
-
-    fn type_name() -> redb::TypeName {
-        redb::TypeName::new("UserInfo")
+        std::cmp::Ordering::Equal
     }
 }
 
-type Username = String;
+pub const SESSIONS: MultimapTableDef<UserID, Session> = MultimapTableDef::new("sessions");
 
-const USER_INFO: TableDef<Username, UserInfo> = TableDef::new("user_info");
+type CollectionID = String;
+
+pub const COLLECTIONS: TableDef<UserID, Vec<CollectionID>> = TableDef::new("collections");
+
+pub const COLLECTION_NAMES: TableDef<CollectionID, String> = TableDef::new("collection_names");
+
+#[key_function]
+pub type CollectionContentKey = (UserID, CollectionID);
+
+pub const COLLECTION_CONTENT: MultimapTableDef<CollectionContentKey, TitleKey> =
+    MultimapTableDef::new("collection_content");
+
+#[derive(Debug, Serialize, Deserialize, RedbJsonValue)]
+pub struct ForgotPassword {
+    pub created_at: DateTime<Utc>,
+    pub code: String,
+}
+
+pub const FORGOT_PASSWORD: TableDef<UserID, ForgotPassword> = TableDef::new("forgot_password");
+
+#[key_function]
+type ProgressKey = (UserID, TitleKey);
+
+pub const PROGRESS: MultimapTableDef<ProgressKey, u32> = MultimapTableDef::new("progress");
