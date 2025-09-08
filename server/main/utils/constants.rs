8a373e22d225c28ef83168b167f1ead4f6e3a95a
Delnegend <kiennguyen19323@gmail.com>
2025-09-08 19:30:03 +0700

    refactor(`server/constants`): set comic info & category info schema to fixed GH links

--- server/main/utils/constants.rs
+++ server/main/utils/constants.rs
@@ -5,11 +5,9 @@ pub const SUPPORTED_IMAGE_FORMATS: [&str; 9] = [
     "avif", "bmp", "gif", "jpeg", "jpg", "png", "tif", "tiff", "webp",
 ];
 
-pub const COMICINFO_SCHEMA: &str =
-    r#"<?xml-model href="https://delnegend.com/comicinfo-2.1-extended.xsd"?>"#;
+pub const COMICINFO_SCHEMA: &str = r#"<?xml-model href="https://raw.githubusercontent.com/anansi-project/comicinfo/0b6e01/drafts/v2.1/ComicInfo.xsd"?>"#;
 pub const COMICINFO: &str = "ComicInfo.xml";
-pub const CATEGORY_INFO_SCHEMA: &str =
-    r#"<?xml-model href="https://delnegend.com/categoryinfo-1.0.xsd"?>"#;
+pub const CATEGORY_INFO_SCHEMA: &str = r#"<?xml-model href="https://raw.githubusercontent.com/Delnegend/Delnegend/c0114e8/categoryinfo-1.0.xsd"?>"#;
 pub const CATEGORYINFO: &str = "CategoryInfo.xml";
 
 pub enum ForgotPasswordLimit {
