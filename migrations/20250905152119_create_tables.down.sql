7bdef7f8eff5283564c1acf1b1d06b63dab8112f
Delnegend <kiennguyen19323@gmail.com>
2025-09-09 00:07:02 +0700

    feat: `migrations`

---
+++ migrations/20250905152119_create_tables.down.sql
@@ -0,0 +1,24 @@
+-- database: ../library/yomuyume.db
+DROP TABLE forgot_password_requests;
+
+DROP TABLE reading_progress;
+
+DROP TABLE collection_titles;
+
+DROP TABLE collections;
+
+DROP TABLE sessions;
+
+DROP TABLE users;
+
+DROP TABLE chapter_covers;
+
+DROP TABLE title_covers;
+
+DROP TABLE pages;
+
+DROP TABLE chapters;
+
+DROP TABLE titles;
+
+DROP TABLE categories;
---
+++ migrations/20250905152119_create_tables.up.sql
@@ -0,0 +1,120 @@
+PRAGMA journal_mode = WAL;
+
+CREATE TABLE
+    categories (
+        id TEXT PRIMARY KEY NOT NULL,
+        path TEXT NOT NULL UNIQUE,
+        parent_id TEXT,
+        name TEXT NOT NULL,
+        description TEXT,
+        FOREIGN KEY (parent_id) REFERENCES categories (id) ON DELETE SET NULL
+    );
+
+CREATE TABLE
+    titles (
+        id TEXT PRIMARY KEY NOT NULL,
+        category_id TEXT,
+        path TEXT NOT NULL UNIQUE,
+        last_modified TIMESTAMP,
+        comic_info BLOB,
+        FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE SET NULL
+    );
+
+CREATE TABLE
+    chapters (
+        id TEXT PRIMARY KEY NOT NULL,
+        title_id TEXT NOT NULL,
+        path TEXT NOT NULL,
+        number INTEGER,
+        last_modified TIMESTAMP,
+        FOREIGN KEY (title_id) REFERENCES titles (id) ON DELETE CASCADE ON UPDATE CASCADE,
+        UNIQUE (title_id, path)
+    );
+
+CREATE TABLE
+    pages (
+        id TEXT PRIMARY KEY NOT NULL,
+        chapter_id TEXT NOT NULL,
+        path TEXT NOT NULL,
+        width INTEGER,
+        height INTEGER,
+        avg_hex_color TEXT,
+        size INTEGER,
+        last_modified TIMESTAMP,
+        UNIQUE (chapter_id, path),
+        FOREIGN KEY (chapter_id) REFERENCES chapters (id) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+
+CREATE TABLE
+    title_covers (
+        title_id TEXT PRIMARY KEY NOT NULL,
+        page_id TEXT NOT NULL,
+        FOREIGN KEY (title_id) REFERENCES titles (id) ON DELETE CASCADE,
+        FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    chapter_covers (
+        chapter_id TEXT PRIMARY KEY NOT NULL,
+        page_id TEXT NOT NULL,
+        FOREIGN KEY (chapter_id) REFERENCES chapters (id) ON DELETE CASCADE,
+        FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    users (
+        id TEXT PRIMARY KEY NOT NULL,
+        username TEXT NOT NULL UNIQUE,
+        email TEXT NOT NULL UNIQUE,
+        password_hash TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        verified_at TIMESTAMP
+    );
+
+CREATE TABLE
+    sessions (
+        secret TEXT PRIMARY KEY NOT NULL,
+        user_id TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    collections (
+        id TEXT PRIMARY KEY NOT NULL,
+        name TEXT NOT NULL,
+        user_id TEXT NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    collection_titles (
+        collection_id TEXT NOT NULL,
+        title_id TEXT NOT NULL,
+        PRIMARY KEY (collection_id, title_id),
+        FOREIGN KEY (collection_id) REFERENCES collections (id) ON DELETE CASCADE,
+        FOREIGN KEY (title_id) REFERENCES titles (id) ON DELETE CASCADE
+    );
+
+CREATE TABLE
+    reading_progress (
+        user_id TEXT NOT NULL,
+        title_id TEXT NOT NULL,
+        chapter_id TEXT,
+        page_id TEXT,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (user_id, title_id),
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+        FOREIGN KEY (title_id) REFERENCES titles (id) ON DELETE CASCADE,
+        FOREIGN KEY (chapter_id) REFERENCES chapters (id) ON DELETE SET NULL,
+        FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE SET NULL
+    );
+
+CREATE TABLE
+    forgot_password_requests (
+        user_id TEXT PRIMARY KEY NOT NULL UNIQUE,
+        code TEXT NOT NULL,
+        created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    );
