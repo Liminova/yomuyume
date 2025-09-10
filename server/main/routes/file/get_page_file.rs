fcd7d17dc6d546a2d55b6b8fcc50b86dff34dda5
Delnegend <kiennguyen19323@gmail.com>
2025-09-10 20:48:43 +0700

    refactor(`server/routes/get_page_file`): update use

--- server/main/routes/file/get_page_file.rs
+++ server/main/routes/file/get_page_file.rs
@@ -3,7 +3,7 @@ use std::sync::Arc;
 use axum::{
     body::Body,
     extract::{Path, State},
-    http::{StatusCode, header},
+    http::{HeaderValue, StatusCode, header},
     response::{IntoResponse, Response},
 };
 use tokio::fs::File;
