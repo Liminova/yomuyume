3a12bd84fba2c3481ae175365a23b132aa9e4749
Delnegend <kiennguyen19323@gmail.com>
2025-09-10 20:31:40 +0700

    refactor(`database/reading_progress`): rm chapter id

--- migrations/20250905152119_create_tables.up.sql
+++ migrations/20250905152119_create_tables.up.sql
@@ -106,14 +106,12 @@ CREATE TABLE
     reading_progress (
         user_id TEXT NOT NULL,
         title_id TEXT NOT NULL,
-        chapter_id TEXT,
-        page_id TEXT,
+        page_id TEXT NOT NULL,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY (user_id, title_id),
         FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
         FOREIGN KEY (title_id) REFERENCES titles (id) ON DELETE CASCADE,
-        FOREIGN KEY (chapter_id) REFERENCES chapters (id) ON DELETE SET NULL,
-        FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE SET NULL
+        FOREIGN KEY (page_id) REFERENCES pages (id) ON DELETE CASCADE
     );
 
 CREATE TABLE
