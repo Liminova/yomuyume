e3a361e9f3f73c325f8895458a89aa3f7f29d035
Delnegend <kiennguyen19323@gmail.com>
2025-09-10 20:45:58 +0700

    refactor(`server/routes/get_cover_file`): update to sqlite w/ new schemas & methods

--- server/main/routes/file/get_cover_file.rs
+++ server/main/routes/file/get_cover_file.rs
@@ -3,136 +3,113 @@ use std::sync::Arc;
 use axum::{
     body::Body,
     extract::{Path, State},
-    http::{StatusCode, header},
+    http::{HeaderValue, StatusCode, header},
     response::{IntoResponse, Response},
 };
 use tokio::fs::File;
 use tokio_util::io::ReaderStream;
+use tracing::error;
 
 use crate::{
-    routes::errors::InternalErr,
-    utils::{app_state::AppState, archive_file::ArchiveFile, constants::GET_COVER_FILE_PATH},
+    AppState,
+    routes::errors::InternalError,
+    utils::{archive_file::ArchiveFile, constants::GET_COVER_FILE_PATH, result_utils::ResultUtils},
 };
 
-/// Get cover file
+/// Get cover file for title
 #[utoipa::path(
     get,
     path = GET_COVER_FILE_PATH,
     responses(
         (status = 200, description = "Fetch cover successful", body = Vec<u8>),
-        (status = 204, description = "Title has no cover"),
-        (status = 401, description = "Unauthorized", body = String),
-        (status = 404, description = "Title not found"),
+        (status = 204, description = "Title/chapter not found or has no cover"),
         (status = 500, description = "Internal server error", body = String),
     ),
     params(
-        ("title_id" = i64, Path, description = "Title ID")
-    ),
-    security(("user-id" = [], "session-secret" = [])))
-]
+        ("title_or_chapter_id" = String, Path, description = "Title or Chapter ID")
+    )
+)]
 pub async fn get_cover_file(
     State(app_state): State<Arc<AppState>>,
-    Path(title_id): Path<i64>,
-) -> Result<Response, InternalErr> {
-    let Some(title) = sqlx::query!(
-        "SELECT path,
-            cover_path,
-            is_dir
-        FROM titles
-        WHERE id = $1",
-        title_id
+    Path(title_or_chapter_id): Path<String>,
+) -> Result<Response, InternalError> {
+    let Some((parent_path, page_path, page_filesize)) = sqlx::query!(
+        "SELECT
+            p.path AS page_path,
+            t.path AS title_path,
+            c.path AS chapter_path,
+            p.size AS page_size
+        FROM
+            pages p
+            JOIN title_covers tc ON p.id = tc.page_id
+            JOIN titles t ON tc.title_id = t.id
+            JOIN chapters c ON t.id = c.title_id
+        WHERE
+            tc.title_id = ?
+        UNION
+        SELECT
+            p.path AS page_path,
+            t.path AS title_path,
+            c.path AS chapter_path,
+            p.size AS page_size
+        FROM
+            pages p
+            JOIN chapter_covers cc ON p.id = cc.page_id
+            JOIN chapters c ON cc.chapter_id = c.id
+            JOIN titles t ON c.title_id = t.id
+        WHERE
+            cc.chapter_id = ?",
+        title_or_chapter_id,
+        title_or_chapter_id
     )
     .fetch_optional(&app_state.pool)
     .await
-    .map_err(|e| {
-        tracing::error!("{e}");
-        InternalErr::DB(e)
-    })?
-    else {
-        return Ok(StatusCode::NOT_FOUND.into_response());
-    };
-
-    let Some(cover_path) = title.cover_path else {
-        return Ok(StatusCode::NO_CONTENT.into_response());
-    };
-
-    let headers = [(
-        header::CONTENT_TYPE,
-        match cover_path
-            .split('.')
-            .next_back()
-            .unwrap_or_default()
-            .to_lowercase()
-            .as_str()
-        {
-            "" => "image".to_string(),
-            "jpg" => "image/jpeg".to_string(),
-            v => format!("image/{v}"),
-        },
-    )];
-
-    let content = match (
-        title.is_dir,
-        cover_path
-            .split_once('/')
-            .map(|(a, b)| (a.to_string(), b.to_string())),
-    ) {
-        (true, Some((chapter_part, page_part))) => {
-            let chapter_path_abs = app_state
-                .config
-                .library_path
-                .as_ref()
-                .join(title.path)
-                .join(chapter_part);
-
-            if chapter_path_abs.is_dir() {
-                File::open(chapter_path_abs.join(page_part))
-                    .await
-                    .map_err(|e| {
-                        tracing::error!("{e}");
-                        InternalErr::IO(e)
-                    })
-                    .map(ReaderStream::new)
-                    .map(Body::from_stream)?
-            } else {
-                chapter_path_abs
-                    .stream_file_from_archive(page_part, None)
-                    .map_err(|e| {
-                        tracing::error!("{e}");
-                        InternalErr::Archive(e)
-                    })
-                    .map(Body::from_stream)?
-            }
-        }
-
-        (true, None) => File::open(
-            app_state
-                .config
-                .library_path
-                .as_ref()
-                .join(&title.path)
-                .join(&cover_path),
-        )
-        .await
-        .map_err(|e| {
-            tracing::error!("{e}");
-            InternalErr::IO(e)
-        })
-        .map(ReaderStream::new)
-        .map(Body::from_stream)?,
-
-        (false, _) => app_state
+    .inspect_err(|e| error!("can't query title/chapter cover: {e}"))?
+    .map(|r| {
+        let parent_path = app_state
             .config
             .library_path
             .as_ref()
-            .join(title.path)
-            .stream_file_from_archive(cover_path.clone(), None)
-            .map_err(|e| {
-                tracing::error!("{e}");
-                InternalErr::Archive(e)
-            })
-            .map(Body::from_stream)?,
+            .join(r.title_path)
+            .join(r.chapter_path);
+
+        (parent_path, r.page_path, r.page_size)
+    }) else {
+        return Ok(StatusCode::NOT_FOUND.into_response());
     };
 
-    Ok((StatusCode::OK, headers, content).into_response())
+    let mut headers = header::HeaderMap::new();
+    if let Some(mime) = page_path
+        .split('.')
+        .next_back()
+        .and_then(mimatcher::mimatcher)
+        .map(HeaderValue::from_static)
+    {
+        headers.insert(header::CONTENT_TYPE, mime);
+    }
+
+    if let Some(size) = page_filesize {
+        headers.insert(header::CONTENT_LENGTH, size.into());
+    }
+
+    let body = if parent_path.is_dir() {
+        let file_path = parent_path.join(page_path);
+
+        let stream = File::open(&file_path)
+            .await
+            .inspect_err(|e| error!("can't open page file {file_path:?}: {e}"))
+            .map(ReaderStream::new)?;
+
+        Body::from_stream(stream)
+    } else {
+        let stream = parent_path
+            .stream_file_from_archive(&page_path)
+            .await
+            .inspect_err(|e| error!("can't open page file {parent_path:?}::{page_path}: {e}"))
+            .map(ReaderStream::new)?;
+
+        Body::from_stream(stream)
+    };
+
+    Ok((StatusCode::OK, headers, body).into_response())
 }
