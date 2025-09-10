10b61b4b8021968c7b6a3726f89f99b78f5803d6
Delnegend <kiennguyen19323@gmail.com>
2025-09-10 20:35:44 +0700

    fix(`server/indexer/index_archive_chap_pages`): sort by path before return

--- server/main/indexer/utils/index_archive_chap_pages.rs
+++ server/main/indexer/utils/index_archive_chap_pages.rs
@@ -91,7 +91,7 @@ pub async fn read_chap_pages_archive(
             })
             .collect::<HashMap<_, _>>();
 
-        partial
+        let mut tmp = partial
             .into_iter()
             .map(|p| {
                 let (width, height, avg_hex_color) = width_height_color
@@ -112,7 +112,10 @@ pub async fn read_chap_pages_archive(
                     size: p.size,
                 }
             })
-            .collect::<Vec<_>>()
+            .collect::<Vec<_>>();
+
+        tmp.sort_by(|a, b| a.path.cmp(&b.path));
+        tmp
     };
 
     Some(IndexedChapterPages {
