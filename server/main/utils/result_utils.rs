8b2ab11fb4e093830fd6de71ebfb5c00e99a0b29
Delnegend <kiennguyen19323@gmail.com>
2025-09-11 23:16:58 +0700

    refactor(`server/utils/result_utils`): rm result's `log_err`; add `inspect_err` for option

--- server/main/utils/result_utils.rs
+++ server/main/utils/result_utils.rs
@@ -1,6 +1,5 @@
 pub trait ResultUtils<T, E> {
     fn okay<F: FnOnce(E)>(self, f: F) -> Option<T>;
-    fn log_err<F: FnOnce(&E)>(self, f: F) -> Result<T, E>;
 }
 
 impl<T, E> ResultUtils<T, E> for Result<T, E> {
@@ -8,23 +7,15 @@ impl<T, E> ResultUtils<T, E> for Result<T, E> {
     fn okay<F: FnOnce(E)>(self, f: F) -> Option<T> {
         self.map_err(f).ok()
     }
-
-    /// Provide a reference error for tracing calls then forward it
-    fn log_err<F: FnOnce(&E)>(self, f: F) -> Result<T, E> {
-        self.map_err(|e| {
-            f(&e);
-            e
-        })
-    }
 }
 
 pub trait OptionUtils<T> {
-    fn log_err<F: FnOnce()>(self, f: F) -> Option<T>;
+    fn inspect_err<F: FnOnce()>(self, f: F) -> Option<T>;
 }
 
 impl<T> OptionUtils<T> for Option<T> {
     /// Provide a closure for tracing calls then forward it
-    fn log_err<F: FnOnce()>(self, f: F) -> Option<T> {
+    fn inspect_err<F: FnOnce()>(self, f: F) -> Option<T> {
         self.or_else(|| {
             f();
             None
